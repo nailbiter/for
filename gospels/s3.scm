0357b13b695ee9dfe13cb784f1ce20dc548cee2a
Alex Leontiev <alozz1991@gmail.com>
2014-01-21 13:04:53 +0800

    Commit

--- gospels/s3.scm
+++ gospels/s3.scm
@@ -5,16 +5,6 @@
 
 (include "s3_aux.scm")
 
-(define (mytokenize regexp str) (define cr (make-regexp regexp))
-  (define (inner str start) 
-    (if (<= (string-length str) start) '()
-	((lambda () (define res (regexp-exec cr str start))
-	   (if (regexp-match? res) 
-	     (if (= (match:start res) start) (inner str (match:end res))
-	  (cons (substring str start (match:start res)) (inner str (match:end res))))
-	  (list (substring str start)))))
-	))
-  (inner str 0))
 (define (replace-in-file in out l) (define lines (mytokenize "\n" (read-delimited "" (open-file in "r"))))
   (define outport (open-file out "w"))
   (define (subst line keyvals) (if (null? keyvals) line (cond 
@@ -34,16 +24,21 @@ i)))(car ll)) (inner (inc i) (cdr ll)))))(inner 0 l))
 					    ))
 				     (mytokenize "\n\n" (myextract filename))))
 
-;(define apo-list 
 (define apo-list (myformatextract "apo.tex"))
-(define apo-data (mytokenize " " (car apo-list)))
-(define apo-list (cdr apo-list))
-(define apo-list  (replace apo-list 8 apo-english-text))
+(define apo-list (let* ((readings-list (parse-russian-title (list-ref apo-list 4))))
+  (replace apo-list 8 (get-apo-english-text (list-ref readings-list 0) (list-ref readings-list 1) (list-ref readings-list 2) 
+                                            (list-ref readings-list 3)))))
 
 (define gosp-list (myformatextract "gosp.tex"))
 
-(display apo-list)
-(newline)(display data)(newline)(display (length apo-data))
+;(display (list-ref apo-list 8))
+;(download2string "http://www.kingjamesbibleonline.org/book.php?book=Ephesians&chapter=2&verse=")
+(display (string-concatenate (list (read-delimited "" (open-input-pipe (string-concatenate (list "wget -O - "
+                                                   "www.google.com")))) " tesi")))
+;(begin (display 42)(newline)(wait 5)(display 777))
+(newline)
+(newline)
+(newline)
 (exit);FIXME
 
 (replace-in-file "apostol.tex" (string-concatenate (list "apostol_week_" (list-ref apo-list 0) ".tex")) (mypairing
--- gospels/s3_aux.scm
+++ gospels/s3_aux.scm
@@ -2,6 +2,34 @@
 (define (dec n) (- n 1))
 (define (inc n) (+ n 1))
 (define (replace l num elem) (if (<= num 0) (cons elem (cdr l)) (cons (car l) (replace (cdr l) (dec num) elem))))
+(define (get-value l key) (if (string=? key (caar l)) (cadr l) (get-value (cdr l) key)))
 
-(define (get-apo-english-text name chapter verse-start verse-end) "englis");TODO
+
+(define (mytokenize regexp str) (define cr (make-regexp regexp))
+  (define (inner str start) 
+    (if (<= (string-length str) start) '()
+	((lambda () (define res (regexp-exec cr str start))
+	   (if (regexp-match? res) 
+	     (if (= (match:start res) start) (inner str (match:end res))
+	  (cons (substring str start (match:start res)) (inner str (match:end res))))
+	  (list (substring str start)))))
+	))
+  (inner str 0))
+(define (wait n) 
+  (do ((curtime (current-time))) ((< (current-time) (+ curtime n))) ))
+(define (download2string url) ;(let ((pid (system (string-concatenate (list "wget -O index.html " url)) )))
+(system (string-concatenate (list "wget -O index.html " url)) ))
+                               ;(open-pipe (string-concatenate (list "wget -O index.html " url)) OPEN_READ)
+                               ;(wait 5)
+                               ;(display 32);(newline)(display 42)
+                               ;(read-delimited "" (open-file "index.html" "r"))
+                                               ;))
+
+(define (roman2arabic romtext) (get-value (list (cons "I" 1))
+                                          romtext))
+
+(define (parse-russian-title text) (map (lambda (s) (format #f "~d" (string-length s)))(mytokenize " *, *" text)))
+
+(define (get-apo-english-text name chapter verse-start verse-end) 
+  (string-concatenate (list name "\n" chapter "\n" verse-start "\n" verse-end)));TODO
 (define gosp-english-text "");TODO
