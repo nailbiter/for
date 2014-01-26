41b81f0c9b40aeecf5ceb02233ac9a8c10a7ec8d
Alex Leontiev <alozz1991@gmail.com>
2014-01-26 19:03:41 +0800

    Commit

--- gospels/s3.scm
+++ gospels/s3.scm
@@ -2,6 +2,8 @@
 (use-modules (ice-9 regex))
 (use-modules (ice-9 popen))
 (use-modules (ice-9 format))
+(use-modules (srfi srfi-1))
+
 
 (include "s3_aux.scm")
 
@@ -14,57 +16,43 @@
   (map (lambda (line)(write-line (subst line l) outport)) lines))
 
 (define (myextract filename) (string-drop-right (read-delimited "" (open-file filename "r")) 1))
-(define (mypairing l) (define (inner i ll) (if (null? ll) '() (cons (cons (string-concatenate (list "mypattern" (list-ref 
-(list "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b")
-i)))(car ll)) (inner (inc i) (cdr ll)))))(inner 0 l))
+(define (mypairing l) (map cons (map (lambda (s) (string-concatenate (list "mypattern" s )))
+	(list "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c" "d" "e" "f")) l))
+;TODO: chinese font
+
 (define (myformatextract filename) 
   (map (lambda (block) 
 					  (if (<= (length (mytokenize "\n" block)) 1) block
 		(string-drop-right (string-concatenate (map (lambda (line) (string-concatenate (list line "\\\\\n"))) (mytokenize "\n" block))) 1)
 					    ))
 				     (mytokenize "\n\n" (myextract filename))))
+(define (myprocessreading reading) (let* (
+					(inner reading)
+					(readings-list (parse-russian-title (list-ref inner 4)))
+					(name (list-ref readings-list 0))
+					(chapter (list-ref readings-list 1))
+					(verse-start (list-ref readings-list 2))
+					(verse-end (list-ref readings-list 3))
+					(inner (replace inner 8 (get-rdg-english-text name chapter verse-start verse-end )))
+					(inner (replace inner 5 (get-eng-title name chapter verse-start verse-end)))
+					(inner (replace inner 6 (get-chi-title name chapter verse-start verse-end)))
+					(inner (replace inner 9 (get-rdg-chinese-text name chapter verse-start verse-end)))
+					  )
+				     inner
+				     ))
 
-(define apo-list (let* (
-                        (inner (myformatextract "apo.tex"))
-                        (readings-list (parse-russian-title (list-ref inner 4)))
-                        (name (list-ref readings-list 0))
-                        (chapter (list-ref readings-list 1))
-                        (verse-start (list-ref readings-list 2))
-                        (verse-end (list-ref readings-list 3))
-                        (inner (replace inner 8 (get-rdg-english-text name chapter verse-start verse-end )))
-                        (inner (replace inner 5 (get-eng-title name chapter verse-start verse-end)))
-                        (inner (replace inner 6 (get-chi-title name chapter verse-start verse-end)))
-                        ) 
-                   inner
-                   ))
-(define gosp-list (let* (
-                        (inner (myformatextract "gosp.tex"))
-                        (readings-list (parse-russian-title (list-ref inner 4)))
-                        (name (list-ref readings-list 0))
-                        (chapter (list-ref readings-list 1))
-                        (verse-start (list-ref readings-list 2))
-                        (verse-end (list-ref readings-list 3))
-                        (inner (replace inner 8 (get-rdg-english-text name chapter verse-start verse-end )))
-                        (inner (replace inner 5 (get-eng-title name chapter verse-start verse-end)))
-                        (inner (replace inner 6 (get-chi-title name chapter verse-start verse-end)))
-                        ) 
-                   inner
-                   ))
+(define apo-list (myprocessreading (myformatextract "apo.tex")))
+(define gosp-list (myprocessreading (myformatextract "gosp.tex")))
 
-(display (list-ref apo-list 8))(newline)
-(display (list-ref apo-list 5))(newline)
-(display (list-ref apo-list 6))(newline)
-(newline)
-(exit);FIXME
+;(display (list-ref apo-list 8))(newline)
+;(display (list-ref apo-list 5))(newline)
+;(display (list-ref apo-list 6))(newline)
+;(display (list-ref gosp-list 9))(newline)
 
-(replace-in-file "apostol.tex" (string-concatenate (list "apostol_week_" (list-ref apo-list 0) ".tex")) (mypairing
-								(map (lambda (i) (if (< i 0) "" (list-ref apo-list i)))
-								(list 0 1 4 7 -1 5 8 2 6 9 3))
-								))
-(replace-in-file "gospel.tex" (string-concatenate (list "gospel_week_" (list-ref gosp-list 0) ".tex")) (mypairing 
-								(map (lambda (i) (if (< i 0) "" (list-ref gosp-list i)))
-								(list 0 1 4 7 -1 5 8 2 6 9 3))
-								))
-(open-pipe (string-concatenate (list "pdflatex -interaction batchmode" " apostol_week_" (list-ref apo-list 0) ".tex" " && " 
+(define (mypermute l) (map (lambda (i) (if (< i 0) "" (list-ref l i)))(list 0 1 4 7 -1 5 8 2 6 9 3 10 11 12)))
+(replace-in-file "apostol.tex" (string-concatenate (list "apostol_week_" (list-ref apo-list 0) ".tex")) (mypairing(mypermute apo-list)))
+(replace-in-file "gospel.tex" (string-concatenate (list "gospel_week_" (list-ref gosp-list 0) ".tex")) (mypairing (mypermute gosp-list)))
+
+(open-pipe (string-concatenate (list "pdflatex -interaction batchmode" " apostol_week_" (list-ref apo-list 0) ".tex" " || " 
 "pdflatex -interaction batchmode" " gospel_week_" (list-ref gosp-list 0) ".tex")) OPEN_WRITE)
 (newline)
--- gospels/s3_aux.scm
+++ gospels/s3_aux.scm
@@ -1,11 +1,22 @@
 (include "../forscheme/misc.scm")
 
+(define rus2chiurl-table (list
+			(cons "Гал" "GAL")
+			(cons "Мк" "MRK");"馬太福音"
+			(cons "Лк" "LUK")
+			(cons "1 Кор" "1CO")
+			(cons "2 Кор" "2CO")
+			(cons "1 Тим" "1TI")
+			(cons "2 Тим" "2TI")
+			))
+
 (define eng_hash (list (list 1 2) (list 3 4)))
 (define (dec n) (- n 1))
 (define (inc n) (+ n 1))
 (define (replace l num elem) (if (<= num 0) (cons elem (cdr l)) (cons (car l) (replace (cdr l) (dec num) elem))))
 (define (get-value l key) (if (null? l) (string-concatenate (list "not found:" key))
                             (if (string=? key (car (car l))) (cdr (car l)) (get-value (cdr l) key))))
+(define (drop-even l) (if (null? l) '() (cons (car l) (drop-even (cddr l)))))
 
 
 (define (mytokenize regexp str) (define cr (make-regexp regexp))
@@ -25,15 +36,27 @@
                                             (cons "I" "1")(cons "II" "2")(cons "III" "3")(cons "IV" "4")(cons "V" "5")
                                             (cons "VI" "6")(cons "VII" "7")(cons "VIII" "8")(cons "IX" "8")(cons "X" "10")
                                             (cons "XI" "11")(cons "XII" "12")(cons "XIII" "13")(cons "XIV" "14")(cons "XV" "15")
+                                            (cons "XVI" "16")(cons "XVII" "17")(cons "XVIII" "18")(cons "XIX" "19")(cons "XX" "20")
                                             )
                                           romtext))
 
 (define (rusname2engname rusname) (get-value (list 
-                   (cons "Гал" "Galatians")(cons "Кор" "Corinthians")
-                   (cons "Мк" "Mark")(cons "1 Кор" "1 Corinthians")
+                   (cons "Гал" "Galatians")
+                   (cons "Мк" "Mark")
+                   (cons "Лк" "Luke")
+		   (cons "1 Кор" "1 Corinthians")
+		   (cons "2 Кор" "2 Corinthians")
+		   (cons "1 Тим" "1 Timothy")
+		   (cons "2 Тим" "2 Timothy")
                                             ) rusname))
 (define (rusname2chiname rusname) (get-value (list 
-                   (cons "Гал" "Galatians");TODO
+                   (cons "Гал" "加拉太書")
+                   (cons "Мк" "馬可福音");"馬太福音" ""
+                   (cons "Лк" "路加福音")
+		   (cons "1 Кор" "哥林多前書")
+		   (cons "2 Кор" "哥林多後書")
+		   (cons "1 Тим" "提摩太前書")
+		   (cons "2 Тим" "提摩太後書")
                                             ) rusname))
 
 (define (parse-russian-title text) (let* ((tokenized1 (mytokenize " *, *" text))
@@ -52,10 +75,54 @@
          (lines (filter (lambda (line) (regexp-match? (string-match "View more translations of" line))) lines))
          (lines (map (lambda(line)(cons (substring (match:substring (string-match ":[0-9]+" line)) 1)(match:suffix (string-match ">" line)))) lines))
          (lines (filter (lambda (pair) (<= (string->number verse-start)(string->number (car pair))(string->number verse-end))) lines))
-         (lines (map (lambda(pair)(string-concatenate (list (cdr pair) "\n"))) lines))
+         (lines (map (lambda(pair)(string-concatenate (list (cdr pair) "\\\\\n"))) lines))
+	 (lines (map (lambda(s) (regexp-substitute/global #f "<[^<>]*>" s 'pre 'post))lines))
          )
         (string-concatenate lines)
     ))
+
+;chinese text
+(define (get-rdg-chinese-url name chapter) (string-concatenate (list "\"http://rcuv.hkbs.org.hk/bible_list.php?dowhat=&is_search=&bible="
+								     (get-value rus2chiurl-table name) "&kw=&chapter=" chapter
+	"&searchkey=0&read_chkRCUV=RCUV&read_chkCUNP=&read_chkBoth=&chk_version=RCUV&godVersionOption=god1&displayOption=list&kw=&bible_testament="
+	"whole&search_topic_from=GEN&search_chapter_from=0&search_topic_to=GEN&search_chapter_to=0&chapter=" chapter "&version=RCUV&bible="
+								     (get-value rus2chiurl-table name) "\""
+								 )))
+(define (get-rdg-chinese-text name chapter verse-start verse-end)
+(let* ((source  (download2string (get-rdg-chinese-url name chapter)))
+         (has-substring (lambda (subs s) (regexp-match? (string-match subs s))))
+	 (before-last (lambda (pat src) (match:prefix (car (last-pair (list-matches pat src))))))
+	 (after-first (lambda (pat src) (match:suffix (string-match pat src))))
+	 (global-eliminate (lambda(pat src)(regexp-substitute/global #f pat src 'pre 'post)))
+       	 (lines (list-tail (mytokenize "<td valign=\"top\" nowrap >" source) 1))
+	 (lines (filter (lambda (s) (has-substring"!--" s)) lines))
+	 (lines (filter (lambda (s) (has-substring"[0-9]+</td>" s)) lines))
+	 (lines(map(lambda(s)(let*((matchs (string-match "[0-9]+</td>" s))(ss(match:substring matchs)))
+			       (cons(string-drop-right ss 5)(match:suffix matchs)))) lines))
+	 (lines (filter (lambda(pair)(<= (string->number verse-start)(string->number (car pair))(string->number verse-end))) lines))
+	 (lines (map(lambda(pair)(cons(car pair)(before-last "</span>"(after-first "-->"(cdr pair)))))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(global-eliminate "<sup>.*</sup>" (cdr pair))))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(global-eliminate "<td valign=\"top\" style=\"line-height:20px;\"><span>" (cdr pair))))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(global-eliminate "<span class='pnpn-SpecialText-ProperName'>" (cdr pair))))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(global-eliminate "<[^<>]*>" (cdr pair))))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(string-drop (cdr pair) 1)))lines))
+	 (lines (map(lambda(pair)(cons(car pair)(string-filter(lambda(char)(and(not (eq? #\newline char))(not(eq? #\tab char))))(cdr pair))))lines))
+	 (lines (map (lambda(pair)(string-concatenate(list(cdr pair)"\\\\\n"))) lines))
+       	 ;(lines (map match:substring (list-matches "<span>[^<>]+</span>" source)))
+       	 ;(lines (map (lambda (s) (substring s 7 (- (string-length s) 9))) lines))
+	 ;(lines (drop-even lines))
+	 ;(start-number (string->number verse-start))
+	 ;(end-number (string->number verse-end))
+	 ;(lines (list-head lines (dec end-number)))
+	 ;(lines (list-tail lines (dec start-number)))
+	 ;(lines (map (lambda (s) (string-concatenate (list s "\\\\\n"))) lines))
+         )
+        (string-concatenate lines)
+    ))
+
+;titles
 (define (get-eng-title name chapter verse-start verse-end) (string-concatenate (list (rusname2engname name) " "  chapter ":"
                                                                                      verse-start " -- "  chapter ":" verse-end ".")))
-(define (get-chi-title name chapter verse-start verse-end) "chi_name_stub")
+(define (get-chi-title name chapter verse-start verse-end) (string-concatenate (list (rusname2chiname name) " "  chapter ":"
+                                                                                     verse-start " -- "  chapter ":" verse-end ".")))
+
