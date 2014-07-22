b09cda85ab0c875b31e8cf30a8b6764383beedd1
Alex Leontiev <alozz1991@gmail.com>
2014-07-22 22:23:19 +0900

    commit

--- misc/cmd.sh
+++ misc/cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-v1="/home/nailbiter/opencv/TLD/01_david/test.avi /home/nailbiter/opencv/TLD/01_david/gt.txt"
-v2="/home/nailbiter/opencv/TLD/02_jumping/test.avi /home/nailbiter/opencv/TLD/02_jumping/gt.txt"
-v3="/home/nailbiter/opencv/TLD/03_pedestrian1/test.avi /home/nailbiter/opencv/TLD/03_pedestrian1/gt.txt"
-v4="/home/nailbiter/opencv/TLD/04_pedestrian2/test.avi /home/nailbiter/opencv/TLD/04_pedestrian2/gt.txt"
-v5="/home/nailbiter/opencv/TLD/05_pedestrian3/test.avi /home/nailbiter/opencv/TLD/05_pedestrian3/gt.txt"
-v6="/home/nailbiter/opencv/TLD/06_car/test.avi /home/nailbiter/opencv/TLD/06_car/gt.txt"
+v1="$HOME/opencv/TLD/01_david/test.avi $HOME/opencv/TLD/01_david/gt.txt"
+v2="$HOME/opencv/TLD/02_jumping/test.avi $HOME/opencv/TLD/02_jumping/gt.txt"
+v3="$HOME/opencv/TLD/03_pedestrian1/test.avi $HOME/opencv/TLD/03_pedestrian1/gt.txt"
+v4="$HOME/opencv/TLD/04_pedestrian2/test.avi $HOME/opencv/TLD/04_pedestrian2/gt.txt"
+v5="$HOME/opencv/TLD/05_pedestrian3/test.avi $HOME/opencv/TLD/05_pedestrian3/gt.txt"
+v6="$HOME/opencv/TLD/06_car/test.avi $HOME/opencv/TLD/06_car/gt.txt"
 r1="165,93,216,147"
 r2="147,110,180,142" 
 r3="48,46,64,111"
@@ -14,8 +14,8 @@ r5="154,102,178,154"
 r6="142,125,232,164"
 INIT_RECT="$r1 $r2 $r3 $r4 $r5 $r6"
 
-./bin/example_tracking_benchmark $v1 $v2 $v3 $v4 $v5 $v6 TLD $INIT_RECT MEDIANFLOW $INIT_RECT
-#./bin/example_tracking_benchmark $v5 MEDIANFLOW $r5 TLD $r5
+#./bin/example_tracking_benchmark $v1 $v2 $v3 $v4 $v5 $v6 TLD $INIT_RECT MEDIANFLOW $INIT_RECT
+./bin/example_tracking_benchmark -s $v5 MEDIANFLOW $r5 TLD $r5
 
 #./bin/c-example-median_flow_v2 $v1 2>&1|grep finish #309
 #./bin/c-example-median_flow_v2 $v2 2>&1|grep finish #36
