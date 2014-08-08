067ca02a74cb081fd131e8d9bc2dc7f564443bec
Alex Leontiev <alozz1991@gmail.com>
2014-08-08 22:43:52 +0300

    Commit

--- proj_JSFC/printtest.js
+++ proj_JSFC/printtest.js
@@ -3,3 +3,27 @@ function removeById(id)
     var element=document.getElementById(tableid);
     element.parentNode.removeChild(element);
 }
+function butt(){
+    alert("gotcha");
+}
+function print_test(lines)
+{
+    {
+        var oNewP = document.createElement("p");
+        var oText = document.createTextNode("length: "+lines.length);
+        oNewP.appendChild(oText);
+        document.body.appendChild(oNewP);
+    }
+    for( var i = 0; i < lines.length; i++ )
+    {
+        var oNewP = document.createElement("p");
+        var oText = document.createTextNode(lines[i]);
+        oNewP.appendChild(oText);
+        document.body.appendChild(oNewP);
+    }
+    var button = document.createElement("button");
+    var buttonText = document.createTextNode("button");
+    button.appendChild(buttonText);
+    button.onclick=butt;
+    document.body.appendChild(button);
+}
