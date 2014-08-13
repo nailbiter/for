5f2b53ee6bbd57e35b7cea408f9588aaecc93c6c
Alex Leontiev <alozz1991@gmail.com>
2014-08-13 13:30:21 +0200

    Commit

--- tests/server_side/test.cpp
+++ tests/server_side/test.cpp
@@ -23,7 +23,7 @@ int main(void)
     printf( "Content-type: application/javascript; charset=utf-8\n\n");
 
     std::string output = "clientCallback(",query;
-#if 0
+#if 1
     query=std::string(getenv("QUERY_STRING"));
 #else
     query="canto.txt";
@@ -41,16 +41,19 @@ void serve_chosen_file(std::string& output,const std::string& filename)
 {
     std::ifstream file(filename.c_str());
     std::string line;
-    output += "{\"lines\":[";
+    //output += "{\"lines\":[";
     if( file.is_open() )
     {
-        getline(file,line);
-        output += ("\"" + line + "\"");
         while( getline(file,line) )
-            output += (" ,\"" + line + "\"");
+	{
+            if( line[0] != '#' ) 
+            	output += line;
+            	//output += (" ,\"" + line + "\"");
+	}
     }
     file.close();
-    output += "]});";
+    //output += "]});";
+    output += ");";
 }
 
 void serve_files(std::string& output)
