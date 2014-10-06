ab95423a5d963099a8659cd4cf4530c007ca43a0
Alex Leontiev <alozz1991@gmail.com>
2014-10-06 16:22:06 +0900

    Commit

--- forjava/fetcher/Factory.java
+++ forjava/fetcher/Factory.java
@@ -3,6 +3,6 @@ public interface Factory
     public List<Header> getStored();
     public boolean remove(Header header);
     public Message getMessage(Header header);
-    public List<Header> download();
+    public List<Header> download()throws Exception;
     public save(Header header);
 }
--- forjava/fetcher/Fetcher.java
+++ forjava/fetcher/Fetcher.java
@@ -1,8 +1,17 @@
 public class Fetcher
 {
     Factory factory_;
+    boolean haveInternet_ = true;
     public Fetcher(Factory factory)
     {
         factory_ = factory;
+        wakeUp();
+    }
+    private void wakeUp()
+    {
+        List<Message> oldm = factory_.getStored(),
+            newm = null;
+        try{ newm = factory_.download(); }
+        catch(Exception e){ haveInternet_ = false; }
     }
 }
