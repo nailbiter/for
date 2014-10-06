6a15b253c5ef4c3f42f32a867a5da15569227d7e
Alex Leontiev <alozz1991@gmail.com>
2014-10-06 21:54:37 +0900

    Commit

--- forjava/fetcher/Factory.java
+++ forjava/fetcher/Factory.java
@@ -1,8 +1,10 @@
+import java.util.*;
 public interface Factory
 {
-    public List<Header> getStored();
-    public boolean remove(Header header);
+    public List<Header> getStored();//used
+    public boolean remove(Header header);//used
     public Message getMessage(Header header);
-    public List<Header> download()throws Exception;
-    public save(Header header);
+    public List<Header> download()throws Exception;//used
+    public boolean saveHeader(Header header);//used
+    public boolean saveMessageAndHeader(Header header, Message message);
 }
--- forjava/fetcher/Header.java
+++ forjava/fetcher/Header.java
@@ -1,7 +1,7 @@
 public interface Header
 {
-    public Message getMessage();
-    public boolean equals(Object obj);
+    public Message getMessage(Factory factory);//if it was null before -- save it, call factory
+    public boolean equals(Object obj);//implement via char string
     public int hashCode();
     public String toDisplayableString();
 }
