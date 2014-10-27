5fe31c594622971cbfec449104f55355a0ecf906
Alex Leontiev <alozz1991@gmail.com>
2014-10-27 13:55:04 +0100

    Commit

--- tests/server_side/test.cpp
+++ tests/server_side/test.cpp
@@ -12,15 +12,24 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include "jsmn.h"
+
+FILE* logfile = NULL;
 
 void get_files(std::vector<std::string>& filenames);
 void serve_files(std::string& output);
 void serve_chosen_file(std::string& output,const std::string& filename);
 void trim(char** start_ptr,char* end);
+void log(const char* s);
+int decode( char** src,char** dst);
+void we_got_json(char* json_s);
 
+//got: with callback
+//{callback:string,command:string,argument:obj}
 int main(void) 
 {
     printf( "Content-type: application/javascript; charset=utf-8\n\n");
+    //logfile = fopen("/home/nailbiter/public_html/log.txt", "w");
 
     std::string output = "clientCallback(",query;
 #if 1
@@ -29,14 +38,25 @@ int main(void)
     query="canto.txt";
 #endif
 
+    char buf[1000];
+    char *p1=(char*)query.c_str(), *p2=buf;
+    decode(&p1,&p2);
+    if( buf[0] == '{' )
+    {
+        we_got_json(buf);
+        return 0;
+    }
+
     if( query.length() == 0 )
     	serve_files(output);
     else
 	    serve_chosen_file(output,query);
     printf("%s\n",output.c_str());
+    //fclose(logfile);
     return 0;
 }
 
+void log(const char* s){ fprintf(logfile,"%s\n",s); }
 void serve_chosen_file(std::string& output,const std::string& filename)
 {
     std::ifstream file(filename.c_str());
@@ -54,6 +74,19 @@ void serve_chosen_file(std::string& output,const std::string& filename)
     file.close();
     //output += "]});";
     output += ");";
+}
+
+void we_got_json(char* json_s)
+{
+    jsmn_parser parser;
+
+    jsmn_init(&parser);
+    jsmntok_t tokens[256];
+    jsmn_parse(&parser, json_s,strlen(json_s), tokens, 256);
+    for( int i = 0; ((int)tokens[i].type!=0) ; i++ )
+    {
+        printf("%d %d %d %d\n",(int)tokens[i].type,tokens[i].start,tokens[i].end,tokens[i].size);
+    }
 }
 
 void serve_files(std::string& output)
@@ -135,5 +168,49 @@ void trim(char** start_ptr,char* end)
     end--;
     while( end[0] == ' ' ) end--;
     end[1] = '\0';
+}
+int decode( char** src,char** dst){
+    for(; **src!='\0' && **src!='&' && **src!='=' ;(*src)++,(*dst)++){
+        if(**src == '%'){
+            (*src)++;   char code1 = **src;
+            (*src)++;   char code2 = **src;
+            if(code1 == '2' && code2== '0') **dst = ' ';
+            else if(code1 == '2' && code2== '1') **dst = '!';//0x21
+            else if(code1 == '2' && code2== '2') **dst = '\"';
+            else if(code1 == '2' && code2== '3') **dst = '#';//0x23                                                                                  
+            else if(code1 == '2' && code2== '4') **dst = '$';//0x24                                                                                  
+            else if(code1 == '2' && code2== '5') **dst = '%';//0x25                                                                                  
+            else if(code1 == '2' && code2== '6') **dst = '&';//0x26                                                                                  
+            else if(code1 == '2' && code2== '7') { **dst = '\''; (*dst)++; **dst = '\'';}//0x27                                                      
+            else if(code1 == '2' && code2== '8') **dst = '(';//0x28                                                                                  
+            else if(code1 == '2' && code2== '9') **dst = ')';//0x29                                                                                  
+            else if(code1 == '2' && code2== 'B') **dst = '+';//0x2B                                                                                  
+            else if(code1 == '2' && code2== 'C') **dst = ',';//0x2C                                                                                  
+            else if(code1 == '2' && code2== 'F') **dst = '/';//0x2F                                                                                  
+            else if(code1 == '3' && code2== 'A') **dst = ':';//0x3A                                                                                  
+            else if(code1 == '3' && code2== 'B') **dst = ';';//0x3B                                                                                  
+            else if(code1 == '3' && code2== 'C') **dst = '<';//0x3C                                                                                  
+            else if(code1 == '3' && code2== 'D') **dst = '=';//0x3D                                                                                  
+            else if(code1 == '3' && code2== 'E') **dst = '>';//0x3E                                                                                  
+            else if(code1 == '3' && code2== 'F') **dst = '?';//0x3F                                                                                  
+            else if(code1 == '4' && code2== '0') **dst = '@';//0x40                                                                                  
+            else if(code1 == '5' && code2== 'B') **dst = '[';//0x5B                                                                                  
+            else if(code1 == '5' && code2== 'C') **dst = '\\';//0x5C                                                                                 
+            else if(code1 == '5' && code2== 'D') **dst = ']';//0x5D                                                                                  
+            else if(code1 == '5' && code2== 'E') **dst = '^';//0x5E                                                                                  
+            else if(code1 == '7' && code2== 'B') **dst = '{';//0x7B                                                                                  
+            else if(code1 == '7' && code2== 'C') **dst = '|';//0x7C                                                                                  
+            else if(code1 == '7' && code2== 'D') **dst = '}';//0x7D                                                                                  
+            else if(code1 == '7' && code2== 'E') **dst = '~';//0x7E                                                                                  
+            else if(code1 == 'A' && code2== '3') { **dst = 0xC2 ; (*dst)++; **dst = 0xA3;}//0x27                                                     
+            else if(code1 == '0' && code2== 'A') **dst = ' ';
+            else if(code1 == '0' && code2== 'D') (*dst)--;                                                                                           
+            else **dst = ' ';                                                                                                                        
+        }else{                                                                                                                                       
+            **dst = **src;                                                                                                                           
+        }                                                                                                                                            
+    }                                                                                                                                                
+    if(**src == '\0') **dst='\0';
+    return 0;                                                                                                                                        
 }
 //.,$w !./tmp.c.exe > out.buf
