c061c2d979fb0ae6a1d68de300d05833ec53746a
Alex Leontiev <alozz1991@gmail.com>
2014-11-10 07:56:39 +0900

    commit

--- gospels/deffile.scm
+++ gospels/deffile.scm
@@ -1,6 +1,6 @@
-(define apostol-filename "apostol_week_21_after_the_Pentecost.tex")
+(define apostol-filename "apostol_week_22_after_the_Pentecost.tex")
 
-(define gospel-filename "gospel_week_21_after_the_Pentecost.tex")
+(define gospel-filename "gospel_week_22_after_the_Pentecost.tex")
 
 (define date "02/11/2014")
 
