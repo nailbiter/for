e173665901921eb94ee0c2185697584c2344e6d3
Alex Leontiev <alozz1991@gmail.com>
2014-11-25 22:57:12 +0900

    Commit

--- gospels/deffile.scm
+++ gospels/deffile.scm
@@ -1,6 +1,6 @@
-(define apostol-filename "apostol_week_23_after_the_Pentecost.tex")
+(define apostol-filename "apostol_week_25_after_the_Pentecost.tex")
 
-(define gospel-filename "gospel_week_23_after_the_Pentecost.tex")
+(define gospel-filename "gospel_week_25_after_the_Pentecost.tex")
 
-(define date "16/11/2014")
+(define date "30/11/2014")
 
--- gospels/update_gospels.scm
+++ gospels/update_gospels.scm
@@ -1,23 +1,32 @@
 (setlocale LC_ALL "")
 (include "../forscheme/misc.scm")
 (include "deffile.scm")
+(use-modules (ice-9 rdelim))
+(use-modules (ice-9 regex))
+(use-modules (ice-9 popen))
+(use-modules (ice-9 format))
+(use-modules (srfi srfi-1))
 
 ;replace html: try one line with local file --> try many lines with local file --> try with remote --> try with that
 (define server-name "inp9822058@alumni.cs.nctu.edu.tw")
 (define apostol-pdf (string-append(string-drop-right apostol-filename 4)".pdf"))
 (define gospel-pdf (string-append(string-drop-right gospel-filename 4)".pdf"))
+(define (separate lines) (string-append (car lines)"\\n"(if(=(length lines)2)(cadr lines)(separate(cdr lines)))))
 
 (define upload-command (string-append "scp " apostol-filename " "gospel-filename " "apostol-pdf" "gospel-pdf" "
                                       server-name ":~/public_html/docs" ))
-(define replace-command (letrec((target-filename "~/public_html/readings.html")
-                              (separate(lambda(lines)(string-append (car lines)"\\n"(if(=(length lines)2)(cadr lines)(separate(cdr lines))))))
+(define replace-command (let*((target-filename "~/public_html/readings.html")
                               (lines(list (string-append "<tr><td align=\"center\"><b>" date "</b></td>")
                                           (string-append "<td align=\"center\"><a href=\"docs/" apostol-pdf "\"")
                                           ">Reading from Epistles. (Чтение из Апостола)</a></td>"
                                           (string-append "<td align=\"center\"><a href=\"docs/"gospel-pdf"\"")
                                           ">Reading from Gospels (Чтение из Евангелие)</a></td></tr>" ))
+                              (proxytxt "test.txt")
+                              (outport (open-file proxytxt "w"))
+                              (remote-cmd (string-append "~/bin/sed -i '$ i\\" (separate lines) "' "target-filename))
+                              (dum (write-line remote-cmd outport))
                               )
-                          (string-append "echo \"" " ~/bin/sed -i '$ i\\" (separate lines) "' "target-filename "\" | ssh "server-name" 'csh -s'" )
+                          (string-append "cat " proxytxt " | ssh "server-name" 'csh -s'" )
                           ))
 
 (display apostol-filename)(newline)
