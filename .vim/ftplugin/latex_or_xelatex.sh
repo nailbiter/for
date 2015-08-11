4207e048fe47844f24e47a722cab872bb9521960
Alex Leontiev <alozz1991@gmail.com>
2015-08-11 12:08:26 +0900

    Commit

---
+++ .vim/ftplugin/latex_or_xelatex.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+echo "I'm alive!"
+if [ -e Makefile ]
+then
+	#echo "Makefile"
+	shift
+	make $*
+else
+	if [ $(grep '^[^%]*usepackage{fontspec}' $1) ]
+	then
+		xelatex $1
+	else
+		if [ $(grep '^\\bibliography{'$2'}' $1) ]
+		then
+			pdflatex -output-directory ~/for/forlatex/foraux $1
+			pdflatex -output-directory ~/for/forlatex/foraux $1
+			bibtex $2
+			pdflatex -output-directory ~/for/forlatex/foraux $1
+		else
+			pdflatex -output-directory ~/for/forlatex/foraux $1
+		fi
+			ln -fs $HOME/for/forlatex/foraux/$2.pdf $HOME/for/forlatex
+		exit
+	fi
+fi
