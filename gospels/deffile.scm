0b6bfc7152e367a39b9eb65e8c6db1a79af082ad
nailbiter@weil <nailbiter@weil>
2015-10-04 21:41:06 +0900

    Commit

--- gospels/deffile.scm
+++ gospels/deffile.scm
@@ -1,6 +1,6 @@
-(define apostol-filename "apostol_week_17_after_the_Pentecost_27_09_2015.tex")
+(define apostol-filename "apostol_week_18_after_the_Pentecost_04_10_2015.tex")
 
-(define gospel-filename "gospel_week_17_after_the_Pentecost_27_09_2015.tex")
+(define gospel-filename "gospel_week_18_after_the_Pentecost_04_10_2015.tex")
 
-(define date "27/09/2015")
+(define date "04/10/2015")
 
