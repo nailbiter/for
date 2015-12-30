4e86a42996dd67fc7945769940f90cc4c7385c16
Alex Leontiev <u406@weil.jp>
2015-12-30 10:36:23 +0900

    commit

--- gospels/deffile.sh
+++ gospels/deffile.sh
@@ -1,3 +1,3 @@
-apostol_filename="apostol_week_29_after_the_Pentecost_20_12_2015.tex"
-gospel_filename="gospel_week_29_after_the_Pentecost_20_12_2015.tex"
-date="20/12/2015"
+apostol_filename="apostol_week_30_after_the_Pentecost_27_12_2015.tex"
+gospel_filename="gospel_week_30_after_the_Pentecost_27_12_2015.tex"
+date="27/12/2015"
