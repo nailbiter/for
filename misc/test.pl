aeda76ffe7c1dadd7f3fa606ec19c8d360d983ad
レオンチエフ　アレックス <aloz1991@gmail.com>
2016-11-06 22:05:35 +0900

    put tags on sections

--- misc/test.pl
+++ misc/test.pl
@@ -11,11 +11,18 @@ open my $info, $file or die "Could not open $file: $!";
 
 while( my $line = <$info>)  
 {
-	if($line =~ /<reference\|([^>]*)>/)
+	my @matches = ($line =~ m/<reference\|([^>]*)>/g);
+	foreach my $match (@matches)
 	{
-		$set_refs->insert( $1 );
+		$set_refs->insert( $match );
 	}
-	if($line =~ /<label\|([^>]*)>/)
+	my @matches = ($line =~ m/<eqref\|([^>]*)>/g);
+	foreach my $match (@matches)
+	{
+		$set_refs->insert( $match );
+	}
+	my @matches = ($line =~ m/<label\|([^>]*)>/g);
+	foreach my $match (@matches)
 	{
 		$set_labels->insert( $1 );
 	}
@@ -24,25 +31,31 @@ close $info;
 print "set_refs has ".$set_refs->size()." elems\n";
 print "set_labels has ".$set_labels->size()." elems\n";
 
-my @diff = $set_refs->difference($set_labels)->members;
+my @diff = sort($set_refs->difference($set_labels)->members);
+my $i = 0;
 print "\ndead links are: \n\n";
 foreach my $number (@diff)
 {
+	$i++;
 	print ("$number\n");
 }
+print "\n". $i."\n";
 
-my @diff = $set_labels->difference($set_refs)->members;
+my @diff = sort($set_labels->difference($set_refs)->members);
 print "\nuseless stats are: \n\n";
+my $i = 0;
 foreach my $number (@diff)
 {
-	if( $number =~ /bib-/ )
+	if( $number =~ /bib-/ ||  $number =~ /^section:/ || $number =~ /^sec:/ || $number eq "def-n-nots:eq-A" || $number eq "CAS:def-CAS" || $number eq "Aqimages" || $number =~ "def-IqN" )
 	{
 		next;
 	}
-	if( $number =~ /sec:/ )
+	if( $number =~ /^P-def:/ )#FIXME
 	{
 		next;
 	}
 	print ("$number\n");
+	$i++;
 }
+print "\n". $i;
 
