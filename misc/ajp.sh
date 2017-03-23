be16b9c21baa4b7afb891ac7ca5a21a3ef17c761
Alex Leontiev <alex@ajp-games.co.jp>
2017-03-23 20:57:47 +0900

    Commit

--- misc/ajp.sh
+++ misc/ajp.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cp ~/stone2/Develop/Projects/Stone/AssetBundles/iOS/testbundle ~/stone2/Develop/Projects/Stone/Assets/Resources/Prefabs/Debug/testbundle.bytes
+#cp ~/stone2/Develop/Projects/Stone/AssetBundles/iOS/testbundle ~/stone2/Develop/Projects/Stone/Assets/Resources/Prefabs/Debug/testbundle.bytes
+cp ~/stone2/Develop/Projects/Stone/AssetBundles/Android/testbundle ~/stone2/Develop/Projects/Stone/Assets/Resources/Prefabs/Debug/Android/testbundle.bytes
+cp ~/stone2/Develop/Projects/Stone/AssetBundles/Windows/testbundle ~/stone2/Develop/Projects/Stone/Assets/Resources/Prefabs/Debug/Windows/testbundle.bytes
