fc5cc78227a9d9943b658293f46c952250fd27e6
レオンチエフ　アレックス <aloz1991@gmail.com>
2017-09-21 09:38:53 +0900

    talks

--- forscheme/timing.scm
+++ forscheme/timing.scm
@@ -1,3 +1,4 @@
+;syntax: ./timing.scm file1 regex1 file2 regex2 ... timing.csv
 (setlocale LC_ALL "")
 (use-modules (ice-9 regex))
 (use-modules (ice-9 common-list))
@@ -11,30 +12,37 @@
 (define startrow 1)
 
 ;;start
-(define labellist (map (lambda(m)(match:substring m 1)) (list-matches "\\\\mytiming\\{([a-z0-9]+)\\}" (read-delimited "" (open-file (list-ref (program-arguments) 1) "r")))))
-(define timingdata
-  (let*
-    ((lines(mytokenize "\n" (read-delimited "" (open-file (list-ref (program-arguments) 2) "r"))))
-    ((lines(mytokenize "\n" (read-delimited "" (open-file (list-ref (program-arguments) 2) "r"))))
-     (lines(list-tail lines 1))
-    (lines(map(lambda(l)(mytokenize "," l))lines))
-    (lines(filter (lambda(i)(> (length i)0))lines))
-    (lines(map(lambda(l)(cons(car l)(list-tail(cdr l)startrow)))lines))
-    )lines)
-  )
+;;(define labellist (map (lambda(m)(match:substring m 1)) (list-matches "\\\\mytiming\\{([a-z0-9]+)\\}" (read-delimited "" (open-file (list-ref (program-arguments) 1) "r")))))
+(define labellist (map (lambda(m)(match:substring m 1)) (list-matches "folded\\|<label\\|([a-z0-9]+)" (read-delimited "" (open-file (list-ref (program-arguments) 1) "r")))))
+;;(define timingdata
+;;  (let*
+;;    ((lines(mytokenize "\n" (read-delimited "" (open-file (list-ref (program-arguments) 2) "r"))))
+;;    ((lines(mytokenize "\n" (read-delimited "" (open-file (list-ref (program-arguments) 2) "r"))))
+;;     (lines(list-tail lines 1))
+;;    (lines(map(lambda(l)(mytokenize "," l))lines))
+;;    (lines(filter (lambda(i)(> (length i)0))lines))
+;;    (lines(map(lambda(l)(cons(car l)(list-tail(cdr l)startrow)))lines))
+;;    )lines)
+;;  ))
 (define (gettimingdata key)((lambda(arg)(if(eq? arg #f)(throw 'eqc)(car(cdr arg))))(find (lambda(item)(string=? key (car item)))timingdata)))
 
 ;;output
 
 ;;;debug
-(if #t
+(if #f
   (begin
     (display labellist)
     (newline)
     (display (length labellist))
     (newline)
     (display (gettimingdata "title"))
-    ))
+    )
+  (begin
+    (display (length labellist))
+    (newline)
+    (display labellist)
+    )
+  )
 ;;(display (gettimingdata "title"))
 ;;(newline)
 ;;(display "hi")
