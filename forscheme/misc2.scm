e2250d1958984dcb486f87d7cf3ba79524f8e319
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-02-01 20:04:44 +0900

    commit

--- forscheme/misc2.scm
+++ forscheme/misc2.scm
@@ -1,10 +1,11 @@
 (use-modules (ice-9 format))
+(use-modules (srfi srfi-1))
 
 (define(seq a b)(if(>= a b)'()(cons a(seq(+ a 1)b))))
-;;generate random times
-;;@arg start -- start in format, e.g. "10:00"
-;;@arg end -- end in format, e.g. "10:00"
-;;@return list of times in same format as start and end
+;;;generate random times
+;;;@arg start -- start in format, e.g. "10:00"
+;;;@arg end -- end in format, e.g. "10:00"
+;;;@return list of times in format say, 10:10 -> '(10 10)
 (define (randtimes start end count)
   (let*
     ((inc(lambda(x)(+ x 1)))
@@ -23,6 +24,22 @@
          (map(lambda(i)(+ startmin(* i step)))(seq 1(inc count)))))
      (minutestotime
        (lambda(minutes)(format #f "~2,'0d:~2,'0d"(quotient minutes 60)(remainder minutes 60)))))
-    (map minutestotime timelist)))
+    (map (lambda(t)(list(quotient t 60)(remainder t 60))) timelist)))
 
-(display(randtimes "10:00" "21:00" 4))
+(map
+  (lambda(l)
+    (format 
+      #t
+      "{\"name\":\"make 5 flashcards\",\"cronline\":\"~a ~a * * *\",\"delaymin\":1080,\"enabled\":true},~%"
+      (second l)
+      (first l)))
+  (randtimes "11:00" "19:00" 3))
+(map
+  (lambda(l)
+    (format 
+      #t
+      "{\"name\":\"practice 5 flashcards\",\"cronline\":\"~a ~a * * *\",\"delaymin\":1080,\"enabled\":true},~%"
+      (second l)
+      (first l)))
+  (randtimes "11:00" "19:00" 4))
+(display(randtimes "9:00" "21:00" 1))
