6e2051642e311216f8f56d8dbebeb94f5dc7e541
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-06-01 11:46:17 +0900

    commit

--- forpython/forsympy/indicialeqn/indicialeqn.py
+++ forpython/forsympy/indicialeqn/indicialeqn.py
@@ -3,7 +3,6 @@
 import sys
 from sympy import *
 from functools import reduce
-from myprintutils import *
 
 x, ell, mu, r = symbols('x ell mu r')
 init_printing(use_unicode=True)
@@ -16,6 +15,23 @@ def indicialeqn(a,b,c,t):
     @param a a(x) function in symbol x
     @param b b(x) function in symbol x
     @param c c(x) function in symbol x
-    @return eqn indicial equation as polynomial in symbol r
+    @param t point at which to construct the equation
+    @return (b,c) coefficients r^2 + b(r)*r + c(r) of indicial equation
     """
-    print("hi")
+    if(t==oo):
+        return indicialeqn(pow(x,4)*a.subs(x,1/x),
+                -pow(x,2)*b.subs(x,1/x)+2*pow(x,3)*a.subs(x,1/x),c.subs(x,1/x),0)
+
+    p = limit(b/a*(x-t),x,t)
+    q = limit(c/a*pow(x-t,2),x,t)
+
+    expr = r*(r-1)+p*r+q
+    expr = expand(expr)
+    expr = Poly(expr,r)
+
+    return (expr.coeff_monomial(r**2),expr.coeff_monomial(r**1),expr.coeff_monomial(r**0))
+
+#######################################################################
+print(indicialeqn(1-pow(x,2),-2*x,ell*(ell+1)-pow(mu,2)/(1-pow(x,2)),1))
+print(indicialeqn(1-pow(x,2),-2*x,ell*(ell+1)-pow(mu,2)/(1-pow(x,2)),-1))
+print(indicialeqn(1-pow(x,2),-2*x,ell*(ell+1)-pow(mu,2)/(1-pow(x,2)),oo))
