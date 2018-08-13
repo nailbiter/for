cb0ff9419129bdd8c03387de6ab5b7a2b2077bbe
レオンチェフ・アレックス <oleksii@dreampirates.co.jp>
2018-08-13 03:28:18 +0100

    add atom

---
+++ .atom/packages/language-mail/spec/language-mail-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageMailView from '../lib/language-mail-view';
+
+describe('LanguageMailView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
