0e65b529fec270ac937c5291c4b7fc5ab66966f8
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-08-21 02:22:18 +0300

    commit

--- forjava/MarieByrd/misc/facecomp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SAVETO=/Users/nailbiter/faces
-PROJECTDIR=/Users/nailbiter/for/forjava/MarieByrd/
-
-mkdir $SAVETO
-echo -e "$SAVETO/comp.sh: $PROJECTDIR/misc/comp.sh\n\tcp $< \$@" | make -f -
-#docker run -p 9000:9000 -p 8000:8000 -v $SAVETO:/faces -t -i bamos/openface /bin/bash
-docker run -p 9000:9000 -p 8000:8000 -v $SAVETO:/faces bamos/openface /faces/comp.sh
