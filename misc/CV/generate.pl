7262d9718a128736f0b68d34d041a159c802dd4a
レオンチェフ・アレックス <oleksii@dreampirates.co.jp>
2018-09-05 10:44:26 +0100

    CV

--- misc/CV/generate.pl
+++ misc/CV/generate.pl
@@ -6,7 +6,17 @@ use Template;
 
 my $vars = {
   PHONEPREFIX => "050",
-  PHONESUFFIX => "5532 0335"
+  PHONESUFFIX => "5532 0335",
+  DATEOFBIRTH => "Date of Birth",#生年月日
+  PHONENUMBER => "Phone Number",#携帯電話番号
+  TLB => "border-left-width:0.0529cm; border-left-style:solid; border-left-color:#000000;",#thick left black
+  TRB => "border-right-width:0.0529cm; border-right-style:solid; border-right-color:#000000;",#thick left black
+  tRB => "border-right-width:0.0176cm; border-right-style:solid; border-right-color:#000000;",#thin right black
+  tBB => "border-bottom-style:solid; border-right-width:0.0176cm;border-bottom-color:#000000;",#thin bottom black
+  TBB => "border-bottom-style:solid; border-bottom-width:0.0529cm;border-bottom-color:#000000;",#thick bottom black
+  BIRTHDATE => 'December 24, 2018',
+  CURRENTADDRESS => 'Address',#現住所
+  NAME => 'Name',
 };
 my $tt = Template->new({
     INCLUDE_PATH => '.',
