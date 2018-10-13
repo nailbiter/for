67c1004d9cff04f8c39a7950cd6157185db8ec3f
Alex Leontiev <alozz1991@gmall.com>
2018-10-14 01:48:27 +0900

    commit

--- forperl/mailparser.pl
+++ forperl/mailparser.pl
@@ -34,7 +34,7 @@ use Date::Parse;
 
 binmode(STDOUT, ":utf8");
 binmode(STDIN, ":utf8");
-#binmode(STDERR, ":utf8");
+binmode(STDERR, ":utf8");
 
 #global const's
 my %FOLDERDATA = (
@@ -44,8 +44,8 @@ my %FOLDERDATA = (
 			my $kmail = $data->{kmail};
 			my $res1 = grep(/$kmail/,@{$hash->{From}});
 			(my $ss,my $mm,my $hh,my $day,my $month,my $year,my $zone) = strptime($hash->{date});
-			printf("month: %d\n",$month);
-			return $res1 && ($month==$data->{month});
+			$year += 1900;
+			return $res1 && ($month==$data->{month}) && ($year==$data->{year});
 		},
 	},
 	"1/Sent Messages" => {
@@ -54,39 +54,49 @@ my %FOLDERDATA = (
 			my $kmail = $data->{kmail};
 			my $res1 = grep(/$kmail/,@{$hash->{To}});
 			(my $ss,my $mm,my $hh,my $day,my $month,my $year,my $zone) = strptime($hash->{date});
-			printf("month: %d\n",$month);
-			return $res1 && ($month==$data->{month});
+			$year += 1900;
+			return $res1 && ($month==$data->{month}) && ($year==$data->{year});
 		},
 	},
 );
 #global var's
+
+#procedures
+sub connectToMailBox{
+	(my $login, my $host,my $password) = @_;
+	my $ssl=new IO::Socket::SSL("$host:imaps");
+	die ("Error connecting - $@") unless defined $ssl;
+	$ssl->autoflush(1);
+	my $imap = Mail::IMAPClient->new(  
+					Socket=>$ssl,
+	#				Debug=>1,
+					Server => $host,
+					User    => $login,
+					Password=> $password,
+					Clear   => 5,   # Unnecessary since '5' is the default
+	#               ...             # Other key=>value pairs go here
+	)       or die "Cannot connect to $host as $login $@";
+	printf(STDERR "connected: %s\n",$imap->Connected());
+	printf(STDERR "authenticated %s\n",$imap->Authenticated());
+	return $imap;
+}
 
 #main
 my $myEmail, my $kEmail, my $month;
 GetOptions(
 	"myemail=s" => \$myEmail,
 	"kemail=s" => \$kEmail,
-	"filtermonth=i" => \$month,
+	"filterdate=s" => \$month, #e.g. 2018-07 means Jul 2018
 );
 $kEmail = $kEmail."\@ms.u-tokyo.ac.jp";
 my $mongoClient = MongoDB->connect();
 my $mailPassword = $mongoClient->ns("admin.passwords")->find_one({key=>"MATHEMAIL"})->{value};
 printf(STDERR "myEmail: %s\nkEmail: %s\npassword: %s\n",$myEmail,$kEmail,$mailPassword);
-my $host = "mail.ms.u-tokyo.ac.jp";
-my $ssl=new IO::Socket::SSL("$host:imaps");
-die ("Error connecting - $@") unless defined $ssl;
-$ssl->autoflush(1);
-my $imap = Mail::IMAPClient->new(  
-				Socket=>$ssl,
-#				Debug=>1,
-                Server => $host,
-                User    => $myEmail,
-                Password=> $mailPassword,
-                Clear   => 5,   # Unnecessary since '5' is the default
-#               ...             # Other key=>value pairs go here
-)       or die "Cannot connect to $host as $myEmail $@";
-printf(STDERR "connected: %s\n",$imap->Connected());
-printf(STDERR "authenticated %s\n",$imap->Authenticated());
+my $imap = connectToMailBox($myEmail,"mail.ms.u-tokyo.ac.jp",$mailPassword);
+my %filterData = (kmail=>$kEmail);
+@filterData{"year","month"} = map {$_+0} split("-",$month);
+$filterData{month}--;
+printf(STDERR "year: %d, month: %d\n",@filterData{"year","month"});
 for(keys(%FOLDERDATA)){
 	my $folderDataItem = $FOLDERDATA{$_};
 	$imap->select($_);
@@ -96,9 +106,8 @@ for(keys(%FOLDERDATA)){
 		$data{subject} = decode('MIME-Header',$imap->subject($_));
 		my $hashref = $imap->parse_headers($_,"From","To");
 		@data{"From","To"} = @{$hashref}{"From","To"};
-		if($folderDataItem->{filter}->({kmail=>$kEmail,month=>$month},\%data))
+		if($folderDataItem->{filter}->(\%filterData,\%data))
 		{
-#			print "\t".decode('MIME-Header',$imap->subject($_))."\n";
 			print "\t".$data{subject}."\n";
 			print Dumper(\%data);
 		}
