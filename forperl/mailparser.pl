8dfcc63404df0bd9967df5c0d7860244dc8aa9c2
Alex Leontiev <alozz1991@gmall.com>
2018-10-18 14:17:54 +0900

    calibre merge

--- forperl/mailparser.pl
+++ forperl/mailparser.pl
@@ -37,6 +37,13 @@ binmode(STDIN, ":utf8");
 binmode(STDERR, ":utf8");
 
 #global const's
+my @MONTHNAMES = (
+	"Jan","Feb",
+	"Mar","Apr","May",
+	"Jun","Jul","Aug",
+	"Sep","Oct","Nov",
+	"Dec",
+);
 my %FOLDERDATA = (
 	"INBOX" => {
 		filter=> sub {
@@ -47,6 +54,10 @@ my %FOLDERDATA = (
 			$year += 1900;
 			return $res1 && ($month==$data->{month}) && ($year==$data->{year});
 		},
+		label => sub {
+			(my $mailinfo) = @_;
+			$mailinfo->{labels} = ["fromK"];
+		},
 	},
 	"1/Sent Messages" => {
 		filter => sub {
@@ -56,6 +67,10 @@ my %FOLDERDATA = (
 			(my $ss,my $mm,my $hh,my $day,my $month,my $year,my $zone) = strptime($hash->{date});
 			$year += 1900;
 			return $res1 && ($month==$data->{month}) && ($year==$data->{year});
+		},
+		label => sub {
+			(my $mailinfo) = @_;
+			$mailinfo->{labels} = ["toK"];
 		},
 	},
 );
@@ -80,17 +95,41 @@ sub connectToMailBox{
 	printf(STDERR "authenticated %s\n",$imap->Authenticated());
 	return $imap;
 }
+sub getEmailInfo{
+	(my $imap,my $id) = @_;
+	my %data;
+	$data{date} = $imap->date( $_ );
+	$data{subject} = decode('MIME-Header',$imap->subject($_));
+	my $hashref = $imap->parse_headers($_,"From","To");
+	@data{"From","To"} = @{$hashref}{"From","To"};
+	return %data;
+}
+sub processEmailInfo{
+	my $dataRef = $_[0];
+	for(("From","To")){
+		@{$dataRef->{$_}} = map {decode('MIME-Header',$_);} @{$dataRef->{$_}};
+	}
+}
+sub filterDataToDateLine{
+	(my $filterDataRef) = @_;
+#	for( $imap->since('01-Jul-2018') ) {
+	my $res = sprintf("%02d-%s-%04d",01,$MONTHNAMES[$filterDataRef->{month}],$filterDataRef->{year});
+	printf(STDERR "filterDataToDateLine: %s\n",$res);
+	return $res;
+}
 
 #main
-my $myEmail, my $kEmail, my $month;
+my $myEmail, my $kEmail, my $month, my $folderName;
 GetOptions(
 	"myemail=s" => \$myEmail,
 	"kemail=s" => \$kEmail,
-	"filterdate=s" => \$month, #e.g. 2018-07 means Jul 2018
+	"dateFilter=s" => \$month, #e.g. 2018-07 means Jul 2018
+	"folder=s" => \$folderName,
 );
 $kEmail = $kEmail."\@ms.u-tokyo.ac.jp";
 my $mongoClient = MongoDB->connect();
 my $mailPassword = $mongoClient->ns("admin.passwords")->find_one({key=>"MATHEMAIL"})->{value};
+my $mongoCollection = $mongoClient->ns("admin.kmails");
 printf(STDERR "myEmail: %s\nkEmail: %s\npassword: %s\n",$myEmail,$kEmail,$mailPassword);
 my $imap = connectToMailBox($myEmail,"mail.ms.u-tokyo.ac.jp",$mailPassword);
 my %filterData = (kmail=>$kEmail);
@@ -99,17 +138,20 @@ $filterData{month}--;
 printf(STDERR "year: %d, month: %d\n",@filterData{"year","month"});
 for(keys(%FOLDERDATA)){
 	my $folderDataItem = $FOLDERDATA{$_};
+	printf(STDERR "folder: %s",$_);
 	$imap->select($_);
-	for( $imap->search('ALL') ) {
-		my %data;
-		$data{date} = $imap->date( $_ );
-		$data{subject} = decode('MIME-Header',$imap->subject($_));
-		my $hashref = $imap->parse_headers($_,"From","To");
-		@data{"From","To"} = @{$hashref}{"From","To"};
+	for( $imap->since(filterDataToDateLine(\%filterData)) ) {
+		my $id = $_;
+		my %data = getEmailInfo($imap,$id);
+		processEmailInfo(\%data);
 		if($folderDataItem->{filter}->(\%filterData,\%data))
 		{
+			$folderDataItem->{label}->(\%data);
 			print "\t".$data{subject}."\n";
 			print Dumper(\%data);
+			$mongoCollection->insert_one(\%data);
+#			$imap->message_to_file("/Users/oleksiileontiev/Downloads/emails/test.txt",$id) 
+#				or die "Could not message_to_file: $@\n";
 		}
 	}
 }
