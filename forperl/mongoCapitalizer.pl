fd7f4de0e1c318dc7cfee4a83d6031a1a4dae604
Alex Leontiev <alozz1991@gmall.com>
2018-11-06 13:54:46 +0900

    bibtool: all fields to capital files changed: A  mongoCapitalizer.pl A  trello.json
    trello card: https://trello.com/c/4VjTDSZm

---
+++ forperl/mongoCapitalizer.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: mongoCapitalizer.pl
+#
+#        USAGE: ./mongoCapitalizer.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 11/06/18 13:54:13
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
