a0a42f7d7933ed0190f15765609c9570b0a32706
Alex Leontiev <alozz1991@gmall.com>
2018-11-07 12:44:36 +0900

    bibtool: all fields to capital files changed:  M mongoCapitalizer.pl
    trello card: https://trello.com/c/4VjTDSZm

--- forperl/mongoCapitalizer.pl
+++ forperl/mongoCapitalizer.pl
@@ -24,9 +24,20 @@ use utf8;
 use Getopt::Long;
 use Data::Dumper;
 use MongoDB;
+use Text::Table;
 
 
 #global const's
+my %METHODS = (
+	capitalize=> {
+		function=>\&capitalize,
+		description=>"capitalize",
+	},
+	help => {
+		description=>"show this message",
+		function=>\&help,
+	}
+);
 {package Safe::Hash;
         require Tie::Hash;
         our @ISA = 'Tie::StdHash';
@@ -37,24 +48,41 @@ use MongoDB;
                 $_[0]{$_[1]}
         }
 }
+#procedures
+sub help{
+	my $tb = Text::Table->new(
+		"method",\' | ',"description",
+	);
+	$tb->add('-----','-------');
+	for(keys(%METHODS)){
+		$tb->add($_,$METHODS{$_}->{description});
+	}
+#	$tb->add('-----','-------');
+	print $tb;
+}
+sub capitalize(){
+	my %cmdline = @_;
+	my $coll = MongoDB->connect()->ns(sprintf("%s.%s",$cmdline{database},$cmdline{collection}));
+	my $allrecords = $coll->find();
+	while(my $next = $allrecords->next){
+		my $id = $$next{_id};
+		printf(STDERR "id: %s\n",$id);
+		my @keys = keys($next);
+		for(@keys){
+			next if ($_ eq "_id");
+			printf(STDERR "\t%s=>%s\n",$_,uc($_));
+			$coll->update_one({"_id"=>$id},{'$set'=>{uc($_)=>$$next{$_}}});
+			$coll->update_one({"_id"=>$id},{'$unset'=>{$_=>""}});
+		}
+	}
+}
 
 #main
 tie my %cmdline => 'Safe::Hash';
 GetOptions(
 	"collection=s" =>\$cmdline{collection},
 	"database=s" =>\$cmdline{database},
+	"method=s" => \$cmdline{method},
 );
 printf(STDERR "%s.%s\n",$cmdline{database},$cmdline{collection});
-my $coll = MongoDB->connect()->ns(sprintf("%s.%s",$cmdline{database},$cmdline{collection}));
-my $allrecords = $coll->find();
-while(my $next = $allrecords->next){
-	my $id = $$next{_id};
-	printf(STDERR "id: %s\n",$id);
-	my @keys = keys($next);
-	for(@keys){
-		next if ($_ eq "_id");
-		printf(STDERR "\t%s=>%s\n",$_,uc($_));
-		$coll->update_one({"_id"=>$id},{'$set'=>{uc($_)=>$$next{$_}}});
-		$coll->update_one({"_id"=>$id},{'$unset'=>{$_=>""}});
-	}
-}
+$METHODS{$cmdline{method}}->{function}->(%cmdline);
