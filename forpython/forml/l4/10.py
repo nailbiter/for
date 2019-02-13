25f8d58212cf22ce9c339637a39f74ae44b0aef6
Alex Leontiev <alozz1991@gmall.com>
2019-02-13 19:36:34 +0900

    read Elix book files changed: AM .gitignore MM l4/10.py AM l4/11.py  M trello.json
    trello card: https://trello.com/c/R64qzGjI

--- forpython/forml/l4/10.py
+++ forpython/forml/l4/10.py
@@ -6,36 +6,38 @@ Created on Tue Jan 22 17:48:40 2019
 @author: oleksiileontiev
 """
 
-import numpy;
+import numpy.random as r;
+import theano.tensor as t;
 import theano;
-import theano.tensor as T;
+import numpy as np;
 import sklearn.metrics;
 
-
-#global const's
+# global const's
 examples = 1000;
 features = 100;
 training_steps = 1000;
-#procedures
-def l2(x):
-    return T.sum(x**2);
 
-#main
-D = (numpy.random.randn(examples, features), numpy.random.randint(size=examples,
-low=0, high=2));
-x = T.dmatrix("x");
-y = T.dvector("y");
-w = theano.shared(numpy.random.randn(features), name="w")
-b = theano.shared(0., name="b")
-p = 1 / (1 + T.exp(-T.dot(x, w) - b))
-error = T.nnet.binary_crossentropy(p,y)
+
+# procedures
+def l2(v):
+    return t.sum(v ** 2);
+
+
+# main
+D = (np.random.randn(examples, features), np.random.randint(size=examples, low=0, high=2));
+x = t.dmatrix("x");
+y = t.dvector("y");
+w = theano.shared(r.randn(features), name="w");
+b = theano.shared(0., name="b");
+p = 1 / (1 + theano.tensor.exp(-theano.tensor.dot(x, w) - b))
+error = theano.tensor.nnet.binary_crossentropy(p, y)
 loss = error.mean() + 0.01 * l2(w)
 prediction = (p > 0.5);
-gw, gb = T.grad(loss, [w, b]);
-train = theano.function(inputs=[x,y],outputs=[p, error], updates=((w, w - 0.1 * gw),
-(b, b - 0.1 * gb)));
+gw, gb = theano.tensor.grad(loss, [w, b]);
+train = theano.function(inputs=[x, y], outputs=[p, error], updates=((w, w - 0.1 * gw),
+                                                                    (b, b - 0.1 * gb)));
 predict = theano.function(inputs=[x], outputs=prediction);
-print("Accuracy before Training:",sklearn.metrics.accuracy_score(D[1], predict(D[0])));
+print("Accuracy before Training:", sklearn.metrics.accuracy_score(D[1], predict(D[0])));
 for i in range(training_steps):
     prediction, error = train(D[0], D[1])
 print("Accuracy after Training:", sklearn.metrics.accuracy_score(D[1], predict(D[0])));
