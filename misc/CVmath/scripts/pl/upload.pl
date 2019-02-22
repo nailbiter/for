e9c5a0fc365e7f01e3f89a559aa911c8ee8e42a8
Alex Leontiev <alozz1991@gmail.com>
2019-02-22 17:17:05 +0900

    JSPS application checklists: TODO 	o (i) What you did. 	o (ii) What you will do. 	o (iii) The list of talks and articles. TODO: backbone 	o set up printing from bib 	o organize access to bib in macbook 	o download from bib in macbook 	o edit bib in macbook TODO: rebuild my webpage 	o upload script 	o rebuilld webpage 	o restore to prev state 	o switch publications_rev to db
    files changed:
     D ../CV/.gitignore
     D ../CV/CV_files/avatar.PNG
     D ../CV/CV_files/css/CV.css
     D ../CV/CV_files/css/CV_engstyle.css
     D ../CV/CV_files/css/mystyle.css
     D ../CV/CV_files/css/workexp.css
     D ../CV/Makefile
     D ../CV/generate.pl
     D ../CV/generateAux.pl
     D ../CV/generateCSS.pl
     D ../CV/json/config_def.json
     D ../CV/json/config_engstyle.json
     D ../CV/json/data.json
     D ../CV/templates/CV.template.xhtml
     D ../CV/templates/CV_engstyle.template.html
     D ../CV/templates/snip1.template.xhtml
     D ../CV/templates/snip2.template.xhtml
     D ../CV/templates/snipli.template.html
     D ../CV/templates/snipsec.template.html
     D ../CV/trello.txt
    A  .gitignore
    AM Makefile
    A  publications_rev.html
    A  publications_unrev.html
    AM scripts/pl/upload.pl
    A  settings.json
    A  speeches.html
    AM trello.json
    A  ../CVprog/.gitignore
    A  ../CVprog/CV_files/avatar.PNG
    A  ../CVprog/CV_files/css/CV.css
    A  ../CVprog/CV_files/css/CV_engstyle.css
    A  ../CVprog/CV_files/css/mystyle.css
    A  ../CVprog/CV_files/css/workexp.css
    A  ../CVprog/Makefile
    A  ../CVprog/generate.pl
    A  ../CVprog/generateAux.pl
    A  ../CVprog/generateCSS.pl
    A  ../CVprog/json/config_def.json
    A  ../CVprog/json/config_engstyle.json
    A  ../CVprog/json/data.json
    A  ../CVprog/templates/CV.template.xhtml
    A  ../CVprog/templates/CV_engstyle.template.html
    A  ../CVprog/templates/snip1.template.xhtml
    A  ../CVprog/templates/snip2.template.xhtml
    A  ../CVprog/templates/snipli.template.html
    A  ../CVprog/templates/snipsec.template.html
    A  ../CVprog/trello.txt
    
    trello card: https://trello.com/c/HjAXGi1a

---
+++ misc/CVmath/scripts/pl/upload.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: upload.pl
+#
+#        USAGE: ./upload.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2019年02月22日 17時14分15秒
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
