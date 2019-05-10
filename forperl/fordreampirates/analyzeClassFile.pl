6227d01ea08ab4f4a4f932c18fac9cf1e658c463
Alex Leontiev <alozz1991@gmail.com>
2019-05-10 20:53:28 +0900

    haishin checklists: TODO 	v why does userApp does not work? 	o https://trello.com/c/HShc1pHB 	o fix compile error TODO: understand 	o https://trello.com/c/JOv97M8N
    files changed:
    AM fordreampirates/.gitignore
    AM fordreampirates/analyzeClassFile.pl
    A  fordreampirates/trello.json
    M  send_slack_msg.pl
     M trello.json
    
    trello card: https://trello.com/c/gZgyxlMO

---
+++ forperl/fordreampirates/analyzeClassFile.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: analyzeClassFile.pl
+#
+#        USAGE: ./analyzeClassFile.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2019年05月10日 18時58分51秒
+#     REVISION: only <= argument, does not see members;
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use Path::Tiny;
+use Data::Dumper;
+
+
+#global const's
+#global var's
+#procedures
+
+#main
+my $argTypePairRegex = '[_a-zA-Z]+:\s*[a-z\.A-Z]+';
+my $regex = 
+	qr/^\s*(?<ACCESS_MODIFIER>private|public)?\s*(async)?\s*(?<FUNC_NAME>[a-zA-Z]+)\s*(=)?\s*(async)?\s*\((?<ARGLIST>\s*($argTypePairRegex)?(,\s*$argTypePairRegex)*\s*)\)/;
+for my $fn (@ARGV) {
+	my $ft = path($fn)->slurp_utf8;
+	for my $line (split("\n",$ft,)) {
+		chomp $line;
+		if($line =~ /$regex/) {
+			printf("%s\n",$line);
+			my @list = $line =~ /$regex/;
+#			printf("res: %s\n%s\n",Dumper(\@list),Dumper(\%+));
+		}
+	}
+}
