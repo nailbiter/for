bd4b27ee8c2c8351dd11dd3419a0812f0679b1e6
Alex Leontiev <alozz1991@gmail.com>
2019-06-04 21:02:26 +0900

    commit

---
+++ forperl/fillTime.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: fillTime.pl
+#
+#        USAGE: ./fillTime.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2019年06月04日 20時50分14秒
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
+#main
---
+++ misc/config.fish
@@ -0,0 +1,7 @@
+set -x PATH $PATH ~/bin /usr/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/TeX/texbin ~/anaconda2/bin
+set -x EDITOR vim
+set -x SHELL /bin/zsh
+#alias rm="rm-p -i"
+function mygit
+    ~/for/forperl/commit.pl
+end
