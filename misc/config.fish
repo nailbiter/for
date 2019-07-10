b9093c104b82e5f82e5ad673455fe419b904d575
Alex Leontiev <alozz1991@gmall.com>
2019-07-10 16:35:31 +0900

    misc config edits

--- misc/config.fish
+++ misc/config.fish
@@ -1,7 +1,16 @@
 set -x PATH $PATH ~/bin /usr/local/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/TeX/texbin ~/anaconda2/bin
 set -x EDITOR vim
 set -x SHELL /usr/local/bin/fish
+
 #alias rm="rm-p -i"
+alias npmr="npm run"
+alias npmb="npm run build"
+alias npmi="npm install"
+alias npmig="npm install -g"
+alias npmci="npm ci"
+alias npmst="npm start"
+alias npmsv="npm run serve"
+
 function mygit
     ~/for/forperl/commit.pl
 end
