5008fcea5bd767231254b8280c866ac021161aff
Alex Leontiev <nailbiter@dtws-work.in>
2019-10-24 12:14:17 +0900

    find articles in journals checklists: TODO: journals 	o 1.1 	o 1.2 	o 1.3 	o 1.4 	o 1.5 	v 2.1 1 	v 2.2 1 	v 2.3 1 	v 2.4 1 	v 2.5 1 	v 3.1 11 	v 3.2 11 	o 3.3 11 	o 3.4 11 	o 3.5 11 	o 4.1 	o 4.2 	o 4.3 	o 4.4 	o 4.5 	o 5.1 	o 5.2 	o 5.3 	o 5.4 	o 5.5 	o 6.1 	o 6.2 	o 6.3 	o 6.4 	o 6.5 	o 7.1 	o 7.2 	o 7.3 	o 7.4 	o 7.5 	o 8.1 	o 8.2 	o 8.3 	o 8.4 	o 8.5 	o 9.1 	o 9.2 	o 9.3 	o 9.4 	o 9.5 	o 10.1 	o 10.2 	o 10.3 	o 10.4 	o 10.5 	o 11.1 	o 11.2 	o 11.3 	o 11.4 	o 11.5 	o 12.1 	o 12.2 	o 12.3 	o 12.4 	o 12.5 	o 13.1 	o 13.2 	o 13.3 	o 13.4 	o 13.5
    files changed:
     M .parseConferencePage.d/.gitignore
     M parseConferencePage.pl
    AM trello.json
    
    trello card: https://trello.com/c/YYbfvKkR

--- forperl/fordatawise/parseConferencePage.pl
+++ forperl/fordatawise/parseConferencePage.pl
@@ -23,6 +23,9 @@ use warnings;
 use utf8;
 use LWP::Simple;
 use Getopt::Long;
+use HTML::PullParser;
+use JSON;
+use Path::Tiny qw(path);
 
 
 #global const's
@@ -75,6 +78,7 @@ my $CONF = {
 		"https://dblp.org/db/conf/webi/webi2019c.html",
 	],
 };
+#procedures
 
 #main
 my %opts;
@@ -96,17 +100,64 @@ if( not @ARGV ) {
 		printf("\t%90s: %04d\n","total",$total);
 		print("\n");
 	}
-} elsif( @ARGV == 1 ) {
-	my $str = get($ARGV[0]);
-	while( $str =~ /<li class="entry inproceedings".*?>(.*?)<\/article>/g ) {
-		if( $1 =~ /.*?<b>view<\/b>.*?href="(?<href>.*?)".*<span class="title" itemprop="name">(?<name>.*?)<\/span>/) {
+} else {
+	for(@ARGV) {
+		my @res;
+		if( /a\d+\.html$/ ) {
+			#Applied Geograghy
+			my $p = HTML::PullParser->new(file => $_,
+									   start => 'event, tagname, @attr',
+									   end   => 'event, tagname, skipped_text',
+									   ignore_elements => [qw(script style)],);
+			while( my $token = $p->get_token) {
+				if( ref($token) eq "ARRAY" && @$token >= 4 && $token->[0] eq "start" && $token->[1] eq "li" && $token->[2] eq "class" && $token->[3] =~ /js-article-list-item/ ) {
+					my $count = 1;
+					my @content;
+					while( ($count > 0) && (my $t = $p->get_token) ) {
+						push @content, $t;
+						if( ref($t) eq "ARRAY" && @$t >= 2 && $t->[1] eq "li" ) {
+							if( $t->[0] eq "start" ) {
+								$count++;
+							} elsif( $t->[0] eq "end" ) {
+								$count--;
+							}
+						}
+					}
+
+					my %article;
+					for(@content) {
+						if( ref($_) eq "ARRAY" && @$_ >= 3 && $_->[2] =~ /select article (.*)/ ) {
+							$article{name} //= $1;
+						}
+					}
+					for(0..scalar(@content)) {
+						if( ref($content[$_]) eq "ARRAY" && @{$content[$_]} >= 3 && $content[$_]->[1] eq "div" && $content[$_]->[2] eq "hidden" ) {
+							$article{href} //= $content[$_+1]->[2];
+						}
+					}
+					push @res, \%article;
+				}
+			}
+			shift @res;
+		} else {
+			my $str = get($_);
+			while( $str =~ /<li class="entry inproceedings".*?>(.*?)<\/article>/g ) {
+				if( $1 =~ /.*?<b>view<\/b>.*?href="(?<href>.*?)".*<span class="title" itemprop="name">(?<name>.*?)\.?<\/span>/) {
+					push @res, {
+						name=>$+{name},
+						href=>$+{href},
+					};
+				}
+			}
+		}
+
+		for( @res ) {
+			my %article = %$_;
 			if( $opts{pretty} ) {
-				printf("%s, %s\n\n",@+{qw(name href)});
+				printf("*%s*, %s\n\n",@article{qw(name href)});
 			} else {
-				printf("%s\n",@+{qw(name)});
+				printf("%s\n",@article{qw(name)});
 			}
 		}
 	}
-} else {
-	die join(" ",@ARGV);
 }
