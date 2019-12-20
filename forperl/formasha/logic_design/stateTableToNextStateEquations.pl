87d557e90daabdd7dfcaf835723ae9076faf6be9
Alex Leontiev <alozz1991@gmail.com>
2019-12-20 15:07:39 +0900

    fixup! Проверить Машин state table для проекта по логическому дизайну (2/4=50.00%)

--- forperl/formasha/logic_design/stateTableToNextStateEquations.pl
+++ forperl/formasha/logic_design/stateTableToNextStateEquations.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/env perl -s
+#!/usr/bin/env perl
 #===============================================================================
 #
-#         FILE: Omj7Yi8o_X.pl
+#         FILE: stateTableToNextStateEquations.pl
 #
-#        USAGE: ./Omj7Yi8o_X.pl  
+#        USAGE: ./stateTableToNextStateEquations.pl  
 #
 #  DESCRIPTION: 
 #
@@ -18,12 +18,25 @@
 #     REVISION: ---
 #===============================================================================
 
+use strict;
 use warnings;
 use utf8;
-#use JSON;
+use JSON;
+use Getopt::Long;
 
 
 #procedures
+sub generate_all_input_combinations {
+	(my @inputs) = @_;
+	my @res;
+	for my $val (0..(2**@inputs-1)) {
+		my @values = reverse map {int($val/(2**$_))%2} (0..$#inputs);
+		my %h;
+		@h{@inputs} = @values;
+		push @res,\%h;
+	}
+	return @res;
+}
 sub gv_new {
 	(my %args) = @_;
 
@@ -56,13 +69,13 @@ sub gv_toString {
 		);
 	}
 
-	for(sort keys %{$self->{vertices}}) {
+	for my $v (sort keys %{$self->{vertices}}) {
 		push @res, sprintf("%s\"%s\" [%s];",
 			"\t" x $tabLevel,
-			$_,
+			$v,
 			join(",", map {
-					sprintf("%s=\"%s\"",$_,$e->{params}->{$_})
-				} sort keys %{$e->{params}}),
+					sprintf("%s=\"%s\"",$_,$self->{vertices}->{$v}->{$_})
+				} sort keys %{$self->{vertices}->{$v}}),
 		);
 	}
 
@@ -76,9 +89,59 @@ sub gv_addEdge {
 		{src=>$src,dst=>$dst,params=>\%params},
 	];
 }
+sub sop_new {
+	(my %args) = @_;
+
+	my $self = {
+	};
+	if( defined $args{sop} ) {
+		$self->{sop} = $args{sop};
+	} elsif(defined $args{expression}) {
+		my @sop;
+#		print STDERR "expression: ",$args{expression},"\n";
+		for(split(/\|\|/,$args{expression})) {
+			/^\s*\(?(?<minterm>[!&a-zA-Z_0-9-\s]*)\)?\s*$/;
+			my %h;
+			for (split(/&&/,$+{minterm})) {
+				/^\s*(?<negation>!)?\s*(?<term>[a-zA-Z_0-9-]+)\s*$/;
+				$h{$+{term}} = $+{negation} ? 0 : 1;
+			}
+			push @sop,\%h;
+		}
+		$self->{sop} = \@sop;
+	}
+	return $self;
+}
+sub sop_eval {
+	(my $self,my %values) = @_;
+	for my $mt ( @{$self->{sop}} ) {
+		my $res = 1;
+		for(keys %$mt) {
+			if( $mt->{$_} != $values{$_} ) {
+				$res = 0;
+				last;
+			}
+		}
+		if( $res ) {
+			return 1;
+		}
+	}
+	return 0;
+}
+sub sop_toString {
+	(my $self,my %args) = @_;
+	$args{or_join_symbol} //= " || ";
+	return join($args{or_join_symbol},map {my $mt = $_;sprintf("(%s)",join(" && ", map {sprintf($mt->{$_}?"%s":"!%s",$_)} sort keys %$mt))} @{$self->{sop}});
+}
 
 #main
-my @lines = <>;
+my @lines = <STDIN>;
+my $check_simplification;
+my $dot;
+GetOptions(
+	"dot"=>\$dot,
+	"check_simplification=s@"=>\$check_simplification,
+);
 map {chomp $_} @lines;
 @lines = map {s/\r//r} @lines;
 (my $header_, my $stateCodes_,my @rest) = @lines;
@@ -101,6 +164,7 @@ print STDERR $stateBitsNum,"\n";
 
 my $first = 1;
 my @res = ([]) x $stateBitsNum;
+my %variables;
 for(@rest) {
 	my @data =  split(/,/);
 	if($data[0] !~ /^$/ && !$first) {
@@ -130,14 +194,28 @@ for(@rest) {
 					$minterm{sprintf("s%d",$j)} = $stateCode[$j];
 				}
 
+				@variables{sort keys %minterm} = (1) x scalar(keys %minterm);
 				push @curArr,\%minterm;
 			}
 			$res[$_] = \@curArr;
 		}
 	}
 }
+@res = map {sop_new(sop=>$_)} @res;
 
-if($dot) {
+if($check_simplification) {
+	my @simplifications = map {sop_new(expression=>$_)} @$check_simplification;
+	print STDERR to_json(\@simplifications),"\n";
+	for my $i (0..$#simplifications) {
+		(my $f1, my $f2) = ($res[$i],$simplifications[$i]);
+		for my $input (generate_all_input_combinations(sort keys %variables)) {
+			print STDERR to_json($input,{canonical=>1}),"\n";
+			if(sop_eval($f1,%$input) != sop_eval($f2,%$input)) {
+				die "not eq ",sop_toString($f2)," ",to_json($input,{canonical=>1}),"=",sop_eval($f2,%$input),"\n";
+			}
+		}
+	}
+} elsif($dot) {
 	my $graph = gv_new;
 #	gv_addVertex($graph,"A");
 #	gv_addVertex($graph,"B");
@@ -169,6 +247,6 @@ if($dot) {
 	print gv_toString($graph);
 } else {
 	for(@res) {
-		print join(" || \n\t",map {my %h=%$_;"( ".join(" & ",map {sprintf($h{$_}?"!%s":"%s",$_)} sort keys %h)." )"} @$_),"\n\n";
+		print sop_toString($_,or_join_symbol=>" || \n\t"),"\n\n";
 	}
 }
