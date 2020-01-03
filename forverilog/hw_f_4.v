42f982d7589624c6c75b67b175663cf915bbd250
Alex Leontiev <alozz1991@gmail.com>
2020-01-03 14:51:24 +0900

    commit

--- forverilog/hw_f_4.v
+++ forverilog/hw_f_4.v
@@ -1,30 +1,54 @@
 `timescale 1ns/1ps
 module hw_f_4;
     reg CLK = 0;
-    reg [1:2] Q;
-    reg X,Z;
+    reg [1:2] Q = 2'b01;
+    reg [1:2] Qp = 2'b01;
+    reg X = 1;
+    wire Z;
+    wire Zp;
     wire [1:2] D;
 
     always
         #5 CLK = ~CLK;
 
     always @(negedge CLK)
-        Q = D;
+        Q = #1 D;
+    always @(negedge CLK)
+        case({Qp,X})
+            3'b000: Qp = #1 2'b10;
+            3'b001: Qp = #1 2'b10;
+            3'b010: Qp = #1 2'b00;
+            3'b011: Qp = #1 2'b11;
+            3'b100: Qp = #1 2'b00;
+            3'b101: Qp = #1 2'b01;
+            3'b110: Qp = #1 2'b01;
+            3'b111: Qp = #1 2'b11;
+        endcase
 
     initial 
         begin
-            $dumpfile("hw_13_5.vcd");
+            $dumpfile("hw_f_4.vcd");
             $dumpvars(0,CLK);
             $dumpvars(0,Q);
+            $dumpvars(0,X);
+            $dumpvars(0,Z);
+            $dumpvars(0,D);
+            $dumpvars(0,Qp);
+            $dumpvars(0,Zp);
 
-            #2 input_data = 15; {EnIn,EnAd,LdAc,LdAd} = 4'b1010;
-            #5 input_data = 13; {EnIn,EnAd,LdAc,LdAd} = 4'b1001;
-            #4 input_data = 15; {EnIn,EnAd,LdAc,LdAd} = 4'b0110;
-            #4 input_data = 93; {EnIn,EnAd,LdAc,LdAd} = 4'b1001;
-            #4 input_data = 47; {EnIn,EnAd,LdAc,LdAd} = 4'b0110;
-            #4 input_data = 22; {EnIn,EnAd,LdAc,LdAd} = 4'b1001;
-            #4 input_data = 0; {EnIn,EnAd,LdAc,LdAd} = 4'b0100;
-            #4 $finish;
+            #4 X = 0;
+            #10 X = 1;
+            #10 X = 1;
+            #10 X = 0;
+            #20 $finish;
         end
+
+    assign #5 D[1] = (~Q[1]&&~Q[2]) || (Q[2]&&X);
+    assign #5 D[2] = &Q || (Q[1]&&X) || (Q[2]&&X);
+    assign #5 Z = Q[1] || (~Q[2]&&X);
+    assign #5 Zp = (Qp==2'b00 && X) ||
+        (Qp==2'b01 && 0) ||
+        (Qp==2'b10 && 1) ||
+        (Qp==2'b11 && 1);
 
 endmodule
