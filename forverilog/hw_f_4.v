ee9035253fb63ddec757b2cbdaa3446dbd04adfc
Alex Leontiev <alozz1991@gmail.com>
2020-01-03 15:11:47 +0900

    test midterm (2/6=33.33%)
    checklists:
      TODO:
              v 1
              v 4
              o 5
              o 2
              o 6
              o 3
    
    URL: https://trello.com/c/xwA6429P

--- forverilog/hw_f_4.v
+++ forverilog/hw_f_4.v
@@ -4,25 +4,25 @@ module hw_f_4;
     reg [1:2] Q = 2'b01;
     reg [1:2] Qp = 2'b01;
     reg X = 1;
-    wire Z;
-    wire Zp;
-    wire [1:2] D;
+    reg Z;
+    reg Zp;
+    reg [1:2] D= 2'b11;
 
     always
         #5 CLK = ~CLK;
 
     always @(negedge CLK)
-        Q = #1 D;
+        Q = #3 D;
     always @(negedge CLK)
         case({Qp,X})
-            3'b000: Qp = #1 2'b10;
-            3'b001: Qp = #1 2'b10;
-            3'b010: Qp = #1 2'b00;
-            3'b011: Qp = #1 2'b11;
-            3'b100: Qp = #1 2'b00;
-            3'b101: Qp = #1 2'b01;
-            3'b110: Qp = #1 2'b01;
-            3'b111: Qp = #1 2'b11;
+            3'b000: Qp = #3 2'b10;
+            3'b001: Qp = #3 2'b10;
+            3'b010: Qp = #3 2'b00;
+            3'b011: Qp = #3 2'b11;
+            3'b100: Qp = #3 2'b00;
+            3'b101: Qp = #3 2'b01;
+            3'b110: Qp = #3 2'b01;
+            3'b111: Qp = #3 2'b11;
         endcase
 
     initial 
@@ -43,12 +43,16 @@ module hw_f_4;
             #20 $finish;
         end
 
-    assign #5 D[1] = (~Q[1]&&~Q[2]) || (Q[2]&&X);
-    assign #5 D[2] = &Q || (Q[1]&&X) || (Q[2]&&X);
-    assign #5 Z = Q[1] || (~Q[2]&&X);
-    assign #5 Zp = (Qp==2'b00 && X) ||
-        (Qp==2'b01 && 0) ||
-        (Qp==2'b10 && 1) ||
-        (Qp==2'b11 && 1);
+    always @(Q,X)
+        begin
+            D[1] <= #5 (~Q[1]&&~Q[2]) || (Q[2]&&X);
+            D[2] <= #5 &Q || (Q[1]&&X) || (Q[2]&&X);
+            Z <= #5 Q[1] || (~Q[2]&&X);
+        end
+    always @(Qp,X)
+        Zp = #5 (Qp==2'b00 && X) ||
+            (Qp==2'b01 && 0) ||
+            (Qp==2'b10 && 1) ||
+            (Qp==2'b11 && 1);
 
 endmodule
