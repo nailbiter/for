98f48254bad38b10effa61562258344afd8c2b50
Alex Leontiev <nailbiter@dtws-work.in>
2020-01-12 15:37:09 +0900

    excise webhook

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-echo $1 | jq -Rc '{text:.}' | curl -X POST -H 'Content-type: application/json' -d @- https://hooks.slack.com/services/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
-#https://hooks.slack.com/services/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
+echo $1 | jq -Rc '{text:.}' | curl -X POST -H 'Content-type: application/json' -d @- $SLACK_WEBHOOK
