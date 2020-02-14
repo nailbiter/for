111be172796cb40c85e2933e5ecd7b9aab7e4934
Alex Leontiev <nailbiter@dtws-work.in>
2020-02-14 21:34:32 +0900

    stash

---
+++ forperl/fordatawise/packages/TrelloWrapper/lib/EHxzFeoHi/TrelloWrapper.pm
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: TrelloWrapper.pl
+#
+#        USAGE: ./TrelloWrapper.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 02/14/2020 21:27:10
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use EHxzFeoHi::Trello;
+
+
+#procedures
+sub new {
+
+}
+ 
+#main
+1;
---
+++ forperl/fordatawise/setTrelloCardKeyword.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: setTrelloCardKeyword.pl
+#
+#        USAGE: ./setTrelloCardKeyword.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 02/14/2020 21:32:34
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use EHxzFeoHi::TrelloWrapper;
+
+
+#main
+print "hi";
