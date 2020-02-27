12eca94a553b98ea72fa9efc6f430dccdcb1463e
Alex Leontiev <nailbiter@dtws-work.in>
2020-02-27 16:21:20 +0900

    fix

--- forperl/fordatawise/bq.pl
+++ forperl/fordatawise/bq.pl
@@ -21,33 +21,50 @@
 use strict;
 use warnings;
 use utf8;
+use JSON;
 
 
 #global const's
-my @PROJECTS = ("astute-cumulus-230103","datawise-0vsyuq974qbk");
+my @PROJECTS = ("astute-cumulus-230103","datawise-0vsyuq974qbk","dtws-rdemo-dev");
+#procedures
+sub myexec {
+    (my $cmd) = @_;
+    print STDERR "> $cmd\n";
+    system($cmd);
+}
 
 #main
-(my $tn, my $cmd) = @ARGV;
+(my $cmd, my $tn) = @ARGV;
 $cmd //= "show";
 $tn //= "";
 
-my $projects = join(",",map {"\"$_\""} @PROJECTS);
-$projects = "[$projects]";
+my $projects = to_json(\@PROJECTS);
 
 $tn =~ /((?<project_id>[a-zA-Z0-9_-]+)(:(?<dataset_id>[a-zA-Z0-9_-]+)(\.(?<table_id>[a-zA-Z0-9_-]+))?)?)?/;
-$tn =~ /((((?<project_id>[a-zA-Z0-9_-]+):)?(?<dataset_id>[a-zA-Z0-9_-]+)\.)?(?<table_id>[a-zA-Z0-9_-]+))?/;
 my %t;
 @t{qw(project_id dataset_id table_id)} = @+{qw(project_id dataset_id table_id)};
 $t{project_id} //= `echo '$projects'|jq -r '.[]'| fzf`;
+die unless $t{project_id};
 chomp $t{project_id};
 
 $t{dataset_id} //= `bq ls --project_id $t{project_id}|fzf`;
+die unless $t{dataset_id};
 chomp $t{dataset_id}; 
 $t{dataset_id} =~ s/^\s+|\s+$//g;
 
-$t{table_id} //= `bq ls $t{project_id}:$t{dataset_id} | fzf | awk -F" " '{print \$1}'`;
-chomp $t{table_id};
-$t{table_id} =~ s/^\s+|\s+$//g;
+my @table_ids;
+if( $t{table_id} ) {
+    @table_ids = ($t{table_id});
+} else {
+    $t{table_id} = `bq ls $t{project_id}:$t{dataset_id} | fzf -m | awk -F" " '{print \$1}'`;
+    die unless $t{table_id};
+    @table_ids = split(/\n/,$t{table_id});
+    map s/^\s+|\s+$//g, map {chomp} @table_ids;
+}
 
 print STDERR join(".",map {"\"$_\""} @t{qw(project_id dataset_id table_id)}),"\n";
-system("bq $cmd $t{project_id}:$t{dataset_id}.$t{table_id}");
+myexec(sprintf("echo '%s'|jq -r '.[]'|gnuparallel bq %s %s:%s.{} :::",
+        to_json(\@table_ids),
+        $cmd,
+        @t{qw(project_id dataset_id)},
+    ));
