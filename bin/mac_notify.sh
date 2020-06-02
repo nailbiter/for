1bf7e5123563ad688b26bed9561c5a38a410e169
Alex Leontiev <nailbiter@dtws-work.in>
2020-06-02 12:25:02 +0900

    crontab

--- bin/mac_notify.sh
+++ bin/mac_notify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-osascript -e "display notification \"$1\" with title \"$1\""
-
+osascript -e 'display notification "'$1'" with title "'$1'"'
+#osascript -e 'tell app "System Events" to display dialog "Hello World"'
