517c5c03d4c45e533aeb3cd0395e8e98e8be5eb8
Alex Leontiev <nailbiter@dtws-work.in>
2020-06-24 20:09:04 +0900

    weekly report

--- bin/weekly-report.sh
+++ bin/weekly-report.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-jupyter nbconvert weekly-analysis.ipynb --to pdf
-du -hs weekly-analysis.pdf
+FN=weekly-analysis
+
+jupyter nbconvert --execute --to notebook --inplace $FN.ipynb
+jupyter nbconvert $FN.ipynb --to pdf
+du -hs $FN.pdf
 DATE=`date "+%Y-%m-%d"`
-mv weekly-analysis.pdf /Users/nailbiter/Google\ Drive/self-management/reports/$DATE-weekly-analysis.pdf
+mv $FN.pdf /Users/nailbiter/Google\ Drive/self-management/reports/$DATE-weekly-analysis.pdf
+git add .
+git commit -a -m "weekly report"
+git push
