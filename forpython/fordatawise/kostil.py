646e26cc856d5e6d396afa0e1e462bc9938b8157
Alex Leontiev <nailbiter@dtws-work.in>
2020-06-26 13:19:44 +0900

    fix schedule 2020-06-19, 2020-06-24 (4/8=50.00%)
    checklists:
      TODO:
              v kostil'
              v fix schedule
              v check schedule fixed
              v `--------------------`
              o light-weight tags
              o remove `kostil.py`
              o flags `:` processor
              o attach url command
    
    URL: https://trello.com/c/azrUSbzN/4870-fix-schedule-2020-06-19-2020-06-24?menu=filter&filter=mac

---
+++ forpython/fordatawise/kostil.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import argparse
+import pymongo
+import pandas as pd
+from pymongo import MongoClient
+import click
+from itertools import product
+
+
+#global const's
+PREFIXES= {"task":"_time_table:task:", "anken":"_time_table:anken:"}
+#procedures
+@click.command()
+@click.argument("task_id",type=str)
+@click.option("--anken",type=click.Choice(['データ収集/天気', 'アルゴリズム改善', 'その他', '推定アルゴリズム/性別推定', 'Datawise Retail', 'covid19']))
+@click.option("--task",type=click.Choice(['その他', '分析・検討・調査', 'レビュー', '資料作成', '会議', 'コーディング']))
+@click.option("--test/--no-test",default=False)
+def kostil(task_id, test, **kwargs):
+    print(f"hi: {task_id}")
+    assert([k for k,v in kwargs.items() if v is not None]), "at least one of `anken` or `task` should be given"
+
+    local_client = MongoClient('localhost', 27017)
+    coll = local_client.admin.passwords
+    doc = coll.find_one({"key":"MONGOMLAB"})
+    mongopass = doc["value"]
+    client = MongoClient(f"mongodb://nailbiter:{mongopass}@ds149672.mlab.com:49672/logistics?retryWrites=false")
+    task_data_coll = client.logistics["alex.taskData"]
+
+    repl = {}
+    doc = task_data_coll.find_one(dict(task_id=task_id))
+    if doc is not None:
+        for kv,t in product(PREFIXES.items(),doc["tags"]):
+            k,v = kv
+            if t.startswith(v):
+                repl[k] = t[len(v):]
+    for k,v in kwargs.items():
+        if v is not None:
+            repl[k] = v
+    
+    repl = [PREFIXES[k]+v for k,v in repl.items()]
+    print(f"repl: {repl}")
+    if not test:
+        task_data_coll.replace_one(dict(task_id=task_id), {"tags":repl,"task_id":task_id}, upsert=True)
+    else:
+        print("skip because of test")
+#main
+kostil()
