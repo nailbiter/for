97a3bccad03aa2b273af34ec55c223c0face1f00
Alex Leontiev <nailbiter@dtws-work.in>
2020-06-29 15:55:37 +0900

    universal reminder script
    URL: https://trello.com/c/7alR7g2D

---
+++ forpython/fordatawise/send_notification.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import click
+from datetime import datetime
+from time import sleep
+from pymongo import MongoClient
+from os import system
+
+
+#procedures
+@click.command()
+@click.argument("message")
+@click.option("--media","-m",multiple=True,default=["slack"],type=click.Choice(["slack","email","popup"],case_sensitive=False))
+@click.option("--delay_min","-d",type=int,default=0)
+def send_notification(message, media,delay_min):
+    print(f"message: {message}")
+    print(f"media: {media}")
+    print(f"delay: {delay_min}")
+
+    print(datetime.now().strftime("%Y-%m-%dT%H:%M"))
+    sleep(delay_min*60)
+
+    if "slack" in media:
+        client = MongoClient()
+        slack_webhook = client.admin.passwords.find_one({"key":"DTWS_SLACK_WEBHOOK"})["value"]
+        #FIXME do not use system
+        system(f"curl -X POST -H 'Content-type: application/json' --data '{{\"text\":\"{message}\"}}' \"{slack_webhook}\"")
+    if "email" in media:
+        raise NotImplementedError
+    if "popup" in media:
+        raise NotImplementedError
+
+    print("\n")
+    print(datetime.now().strftime("%Y-%m-%dT%H:%M"))
+
+#main
+send_notification()
