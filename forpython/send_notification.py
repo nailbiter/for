804f205b1ce61eb651668f01b46347e836f0cdb4
Alex Leontiev <nailbiter@dtws-work.in>
2020-06-30 14:30:09 +0900

    send_notification

--- forpython/send_notification.py
+++ forpython/send_notification.py
@@ -6,24 +6,36 @@ from pymongo import MongoClient
 from os import system
 
 
-#procedures
+# procedures
 @click.command()
 @click.argument("message")
-@click.option("--media","-m",multiple=True,default=["slack"],type=click.Choice(["slack","email","popup"],case_sensitive=False))
-@click.option("--delay_min","-d",type=int,default=0)
-def send_notification(message, media,delay_min):
+@click.option("--media", "-m", multiple=True, default=["slack"], type=click.Choice(["slack", "email", "popup"], case_sensitive=False))
+@click.option("--delay_min", "-d", type=int, default=0)
+@click.option("--silent/--no-silent", default=False)
+def send_notification(message, media, delay_min, silent):
     print(f"message: {message}")
     print(f"media: {media}")
     print(f"delay: {delay_min}")
 
     print(datetime.now().strftime("%Y-%m-%dT%H:%M"))
-    sleep(delay_min*60)
+    if silent:
+        sleep(delay_min*60)
+    else:
+        for i in range(delay_min*60,0,-1):
+            sleep(1)
+            d = datetime.utcfromtimestamp(i,)
+            if d.day>1:
+                print(d.strftime("%Y-%m-%dT%H:%M:%S"))
+            else:
+                print(d.strftime("%H:%M:%S"))
 
     if "slack" in media:
         client = MongoClient()
-        slack_webhook = client.admin.passwords.find_one({"key":"DTWS_SLACK_WEBHOOK"})["value"]
-        #FIXME do not use system
-        system(f"curl -X POST -H 'Content-type: application/json' --data '{{\"text\":\"{message}\"}}' \"{slack_webhook}\"")
+        slack_webhook = client.admin.passwords.find_one(
+            {"key": "DTWS_SLACK_WEBHOOK"})["value"]
+        # FIXME do not use system
+        system(
+            f"curl -X POST -H 'Content-type: application/json' --data '{{\"text\":\"{message}\"}}' \"{slack_webhook}\"")
     if "email" in media:
         raise NotImplementedError
     if "popup" in media:
@@ -32,5 +44,6 @@ def send_notification(message, media,delay_min):
     print("\n")
     print(datetime.now().strftime("%Y-%m-%dT%H:%M"))
 
-#main
+
+# main
 send_notification()
