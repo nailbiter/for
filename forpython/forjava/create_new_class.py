46ce4f686e9fc75a00d76d5297fee038beda8128
Alex Leontiev <nailbiter@dtws-work.in>
2020-07-12 14:01:17 +0900

    beerbot milestone (32/39=82.05%)
    checklists:
      TODO::
              o add error message with Telegram of sender
    
      TODO::infra:
              v single-state-machine architecture infra `#55`
              v be able to follow clean interaction with every user
              v set "commit variable" via env vars, not via cmdline args infra `#68`
              v inline keyboard #66
              v identify keyboards in log infra `#63`
              v telegram keyboard answer indicate keyboard id (error message)
              o ignore press on old keyboard infra `#62`
              v move `UserData` to `InputMessage`, create `ElDrinkoInputMessage` type
    
      TODO::infra::single-state-machine architecture infra `#55`::pure fromJSON:
              v embed database state in TelegramMessage (and separate state db and userRecord db)
              v single state architecture
              v move UserData from message to _execute
              v pure fromJSON
              v start ~~> choose_product_to_see_description
              v choose_product_to_see_description ~~> start
              v start ~~> choose_product_to_make_order
              v choose_product_to_make_order ~~> choose_amount
              v choose_amount ~~> confirm
              v confirm ~~> choose_product_to_make_order
              v confirm ~~> delete
              v delete ~~> confirm
              v confirm ~~> choose_address
              v choose_address ~~> choose_phone_number
              v choose_phone_number ~~> choose_phone_number
              v choose_phone_number ~~> choose_payment
              v choose_payment ~~> send
              v send ~~> edit_address
              v edit_address ~~> send
              v send ~~> choose_payment
              v send ~~> idle
              v idle ~~> start
              v ~~> start
    
      FIXME:
              v embedded buttons
              o `/start` from any state doesn't work
    
      POSTTODO:
              o refactor production db
              o rewrite `run.sh`
              v remove `setUp` (and maybe `finalize`)
    
      POSTTODO: after merge:
              o testing
              o test: state_machine's final configuration
    
    URL: https://trello.com/c/AnKsSrOM/5014-beerbot-milestone?menu=filter&filter=label:coding,label:beerbot

--- forpython/forjava/create_new_class.py
+++ forpython/forjava/create_new_class.py
@@ -3,44 +3,44 @@ from argparse import ArgumentParser
 from jinja2 import Template
 from os import path, system
 import re
+import click
 
 
 # global const's
 # global var's
 # procedures
+@click.command()
+@click.argument("filename",type=click.Path(dir_okay=False,writable=True))
+@click.option("-m","--maven",is_flag=True)
+@click.option("-d","--dry_run",is_flag=True)
+@click.option("-i","--interface",is_flag=True)
+@click.option("-g","--github",is_flag=True)
+@click.option("-e","--extends",type=click.Path(exists=True,dir_okay=False,readable=True))
+def create_new_class(filename,maven,dry_run,interface,github,extends):
+    directory, basename = path.split(__file__)
+    fn_directory, fn_basename = path.split(filename)
+    if maven:
+        m = re.match(r"src/main/java/(.*)", fn_directory)
+        assert(m)
+        fn_directory = m.group(1)
+    class_name, _ = path.splitext(fn_basename)
+    with open(f"{directory}/templates/new_class.jinja.java") as template_file:
+        template = Template(template_file.read())
+    env = {
+        "package_name": fn_directory.replace("/", "."),
+        "class_name": class_name,
+        "is_interface": interface
+    }
+    if extends:
+        env["extends"] = path.splitext(path.split(extends)[1])[0]
+    res = template.render(env)
+    if dry_run:
+        print(res)
+    else:
+        with open(filename, "w") as f:
+            f.write(res)
+            if(github):
+                system(f"git add {filename}")
 
 # main
-parser = ArgumentParser(description="Process some integers.")
-parser.add_argument("filename")
-parser.add_argument("--dry_run", "-d", action="store_true")
-parser.add_argument("--maven", "-m", action="store_true")
-parser.add_argument("--interface", "-i", action="store_true")
-parser.add_argument("--github", "-g", action="store_true")
-parser.add_argument("--extends", "-e")
-
-args = parser.parse_args()
-
-directory, basename = path.split(__file__)
-fn_directory, fn_basename = path.split(args.filename)
-if(args.maven):
-    m = re.match(r"src/main/java/(.*)", fn_directory)
-    assert(m)
-    fn_directory = m.group(1)
-class_name, _ = path.splitext(fn_basename)
-with open(f"{directory}/templates/new_class.jinja.java") as template_file:
-    template = Template(template_file.read())
-env = {
-    "package_name": fn_directory.replace("/", "."),
-    "class_name": class_name,
-    "is_interface": args.interface
-}
-if args.extends:
-    env["extends"] = path.splitext(path.split(args.extends)[1])[0]
-res = template.render(env)
-if args.dry_run:
-    print(res)
-else:
-    with open(args.filename, "w") as f:
-        f.write(res)
-        if(args.github):
-            system(f"git add {args.filename}")
+create_new_class()
