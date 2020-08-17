39cfd9d00bff614eb14ee8d7451236742be7e5e4
Alex Leontiev <nailbiter@dtws-work.in>
2020-08-17 17:32:26 +0900

    stopwatch with shell
    I just realized that I can do stopwatch as pure command-line app,
    without need for shell. So now I will do so

--- forpython/stopwatch.py
+++ forpython/stopwatch.py
@@ -2,29 +2,61 @@
 import readline
 import logging
 from _stopwatch.simple_completer import SimpleCompleter
-from _stopwatch.stopwatch_interpreter import StopwatchInterpreter
+from _stopwatch.interpreter import interpreter, argument
+logging.basicConfig(level=logging.DEBUG,handlers=[logging.FileHandler('.completer.log.txt'),])
 
-LOG_FILENAME = '/tmp/completer.log'
-logging.basicConfig(level=logging.DEBUG,
-#                    filename=LOG_FILENAME,
-                    )
+# global const's
+# global var's
+# procedures
 
 
 def input_loop(stopwatch_interpreter):
     line = ''
     should_continue = True
     while should_continue:
-        line = input('("stop" to quit)> ').strip()
+        line = input(stopwatch_interpreter.prompt).strip()
         should_continue = stopwatch_interpreter.dispatch(line)
 
 
-stopwatch_interpreter = StopwatchInterpreter()
+@interpreter('("stop" to quit)> ')
+def cli():
+    pass
+
+@cli.command("stop")
+@argument("name")
+def stop(name):
+    #TODO
+    print(f"stopping {name}")
+
+@cli.command("start")
+@argument("name")
+def start(name):
+    pass
+
+@cli.command("list")
+def list():
+    #TODO
+    pass
+
+@cli.command("remove")
+@argument("name")
+def remove(name):
+    #TODO
+    pass
+
+@cli.command("reset")
+@argument("name")
+def reset(name):
+    #TODO
+    pass
+#, help
+
+# main
 # Register our completer function
-readline.set_completer(SimpleCompleter(
-    ["start", "stop", "list", "remove", "reset", "help"]).complete)
+readline.set_completer(SimpleCompleter(cli.command_names).complete)
 
 # Use the tab key for completion
 readline.parse_and_bind('tab: complete')
 
 # Prompt the user for text
-input_loop(stopwatch_interpreter)
+input_loop(cli)
