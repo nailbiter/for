1b2346d3745a7961e5a6a1979c3132c2e085dde8
Alex Leontiev <nailbiter@dtws-work.in>
2020-09-06 20:12:21 +0900

    music-player
    implemented `--recursive`

--- forpython/for-music-player/server.py
+++ forpython/for-music-player/server.py
@@ -3,11 +3,22 @@ from pymongo import MongoClient
 from datetime import datetime
 import click
 import pandas as pd
-
+import logging
+from tempfile import mkstemp
+from bs4 import BeautifulSoup
+import requests
 
 # procedures
 def _get_coll():
     return MongoClient()["for-music-player"].queue
+
+
+def _insert_in_queue(obj, dry_run):
+    logger = logging.getLogger("_insert_in_queue")
+    queue = _get_coll()
+    logger.info(f"insert {obj} in {queue}")
+    if not dry_run:
+        queue.insert_one(obj)
 
 
 @click.group()
@@ -17,10 +28,18 @@ def cli():
 
 @cli.command()
 @click.argument("path")
-def add(path):
-    # FIXME: "recursive" key
-    _get_coll().insert_one(
-        {"path": path, "date": datetime.now(), "played": False})
+@click.option("-r", "--recursive", is_flag=True)
+@click.option("-d", "--dry_run", is_flag=True)
+def add(path, recursive=False, dry_run=False):
+    if recursive:
+        req = requests.get(path)
+        soup = BeautifulSoup(req.text, "html.parser")
+        paths = [path+a['href'] for a in soup.find_all("a")]
+    else:
+        paths = [path]
+    for _path in paths:
+        _insert_in_queue(
+            {"path": _path, "date": datetime.now(), "played": False}, dry_run)
 
 
 @cli.command()
@@ -32,9 +51,12 @@ def list():
 
 # FIXME: "move up and down" command
 # FIXME: add random command
+# FIXME: add length to lib
+# FIXME: buffer
 
 
 # main
 if __name__ == "__main__":
     pd.set_option('max_colwidth', None)
+    logging.basicConfig(level=logging.INFO)
     cli()
