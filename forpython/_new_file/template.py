b58bd6e5f213c4aa2777351ea830d521e01747c3
Alex Leontiev <nailbiter@dtws-work.in>
2020-10-01 11:47:05 +0900

    add author etc comment at every new python file you start
    URL: https://trello.com/c/OSTJVEK7

---
+++ forpython/_new_file/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: {{filename}}
+
+       USAGE: ./{{filename}}
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev ({{email}}),
+ORGANIZATION: 
+     VERSION: 0.0.1
+     CREATED: {{now.isoformat()}}
+    REVISION: ---
+
+==============================================================================="""
---
+++ forpython/new_file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from os.path import realpath, split, basename, join
+from jinja2 import Template
+import click
+from datetime import datetime
+
+
+def _render_template(fn, **kwargs):
+    dirname, _ = split(realpath(__file__))
+    with open(join(dirname, "_new_file", fn)) as f:
+        return Template(f.read()).render(kwargs)
+
+
+@click.command()
+@click.argument("fn", type=click.Path())
+@click.option("-s", "--stdout", is_flag=True)
+@click.option("-e", "--email", envvar="EMAIL",default="alozz1991@gmail.com")
+def new_file(fn, email, stdout=False):
+    s = _render_template("template.py", filename=fn, now=datetime.now(), email=email)
+    if stdout:
+        print(s)
+    else:
+        with open(fn, "w") as f:
+            f.write(s)
+
+
+if __name__ == "__main__":
+    new_file()
