af3b4587ec1d70ca4e05c041ab4cb012c4c2aebd
Alex Leontiev <nailbiter@dtws-work.in>
2020-10-02 20:20:03 +0900

    flashcard-app

--- forpython/flashcard-app/flashcards.py
+++ forpython/flashcard-app/flashcards.py
@@ -6,23 +6,23 @@
        USAGE: ./flashcards.py test
               insert word: ./flashcards.py --tags japanese --tags reading add-item "運用" --back "practical use"
 
- DESCRIPTION: 
+ DESCRIPTION:
 
      OPTIONS: ---
 REQUIREMENTS: ---
         BUGS: ---
        NOTES: ---
       AUTHOR: Alex Leontiev (alozz1991@gmail.com)
-ORGANIZATION: 
+ORGANIZATION:
      VERSION: 0.0.1
      CREATED: 2020-09-24 11:19 +0900
     REVISION: ---
         TODO:
               1. type-in question type
-              2. `show_score`: `--agg` key
+              2(done). `show_score`: `--agg` key
               3. `--(no)-hint` key for test``
               4. 3c043fd172238823
-              5. show score for each deck (dep: 2.)
+              5(done). show score for each deck (dep: 2.)
 
 ==============================================================================="""
 import click
@@ -34,11 +34,14 @@ from itertools import product
 import logging
 from _flashcards.question import get_question_types, get_question
 import json
+from math import ceil
+
+
+_GROUPBY = ["_id", "is_front_to_back", "back_index"]
 
 
 def _get_deck_with_score(deck):
     _logger = logging.getLogger("_get_deck_with_score")
-    _groupby = ["_id", "is_front_to_back", "back_index"]
 
     deck_df = pd.DataFrame(deck)
     deck_df["_id"] = deck_df["_id"].apply(str)
@@ -47,12 +50,12 @@ def _get_deck_with_score(deck):
                       for back_index, is_front_to_back in product(range(len(r["back"])), [True, False])])
         for r
         in deck_df.to_dict(orient="records")
-    ]).set_index(_groupby)
+    ]).set_index(_GROUPBY)
 
     results_df = pd.DataFrame(MongoClient().alex_flashcards.results.find())
     results_df["_id"] = results_df["card"]
     results_df.drop(columns=["card"])
-    results_df = results_df.groupby(_groupby).mean()
+    results_df = results_df.groupby(_GROUPBY).mean()
     _logger.info(f"results_df: {results_df}")
 
     deck_df = deck_df.join(results_df, how="left")
@@ -101,6 +104,107 @@ def flashcards(ctx, tags, debug=False):
 
 
 @flashcards.command()
+@click.option("--deck_size", type=int, default=5, envvar="DECK_SIZE")
+@click.option("--deck_index", type=int, default=-1, envvar="DECK_INDEX")
+@click.option("--full/--no-full", default=False)
+@click.option("--sort/--no-sort", default=False)
+@click.option("--agg", type=click.Choice([*_GROUPBY, "all"]), multiple=True)
+@click.pass_context
+def show_score(ctx, deck_index, deck_size, full, sort, agg):
+    _logger = logging.getLogger("show_score")
+    cards = ctx.obj["cards"]
+    deck_count = ceil(len(cards)/deck_size)
+    _logger.info(f"{deck_count} decks")
+    assert deck_index >= -1
+    if "all" in agg:
+        agg = _GROUPBY
+
+    deck = []
+    for _deck_index in range(deck_count):
+        deck.extend([{**card, "deck_index": _deck_index}
+                     for card in cards[deck_size*_deck_index:(_deck_index+1)*deck_size]])
+    # _logger.info(f"deck: {deck}")
+    if deck_index >= 0:
+        deck = [card for card in deck if card["deck_index"] == deck_index]
+    assert(len(deck) > 0)
+    deck = [{k: v for k, v in c.items() if k not in ["tags"]}
+            for c in deck]
+    _logger.info(f"deck:\n{pd.DataFrame(deck)}")
+
+    deck_df = _get_deck_with_score(deck)
+
+    if len(agg) > 0:
+        deck_df = deck_df.reset_index()
+        _logger.info(f"columns: {list(deck_df)}")
+        _all = {"deck_index", *_GROUPBY}
+        _logger.info(f"_all: {_all}")
+        _agg = list(_all-{*agg})
+        _logger.info(f"_agg: {_agg}")
+
+        def _aggregator(x):
+            idx, df = x
+            __logger = _logger.getChild("_aggregator")
+#            __logger.info(f"idx: {idx}")
+            __logger.info(f"columns: {list(df)}")
+#            __logger.info(f"df: \n{df}")
+            return pd.DataFrame([{
+                **{k: v for k, v in zip(_agg, idx if len(_agg) > 1 else [idx])},
+                **{k: v.mean() if k == "score" else v.sample() for k, v in df.items() if k not in _all},
+            }])
+
+        deck_df=pd.concat(map(_aggregator, deck_df.groupby(
+            _agg, as_index=False))).reset_index().set_index(_agg)
+
+    if not full:
+        deck_df=deck_df.drop(columns=["back", "front"])
+    else:
+        for i in range(deck_df["back"].apply(len).max()):
+            deck_df[f"back[{i}]"]=deck_df["back"].apply(
+                lambda back: None if len(back) <= i else back[i])
+        deck_df=deck_df.drop(columns=["back"])
+
+    if sort:
+        deck_df=deck_df.sort_values(by="score", ascending=False)
+
+    # deck_df = deck_df.drop_duplicates()
+    print(deck_df.to_string(justify="left", col_space=20))
+    print(f"average score: {deck_df['score'].mean()*100:05.2f}%")
+
+
+@ flashcards.command()
+@ click.option("--deck_size", type=int, default=5, envvar="DECK_SIZE")
+@ click.option("--deck_index", type=int, default=-1, envvar="DECK_INDEX")
+@ click.pass_context
+def test(ctx, deck_index, deck_size):
+    _logger=logging.getLogger("test")
+    cards=ctx.obj["cards"]
+    _logger.info(f"{int(len(cards)/deck_size)+1} decks")
+    assert deck_index >= 0
+
+    deck=cards[deck_size*deck_index:deck_size*(deck_index+1)]
+    deck=[{k: v for k, v in c.items() if k not in ["tags"]}
+            for c in deck]
+    assert(len(deck) > 0)
+    _logger.info(f"deck:\n{pd.DataFrame(deck)}")
+    question_i=0
+    while True:
+        question_i += 1
+        _d=_get_random_question(deck)
+        question=get_question(
+            _d["question_type"], **{k: v for k, v in _d.items() if k != "question_type"})
+        print(f"question #{question_i}: \n{question.get_question_text()}")
+        while True:
+            res, msg=question.grade(input("answer> "))
+            if msg is not None:
+                print(f"({msg})")
+            if res is not None:
+                break
+        print(f"res: {res}")
+        obj=json.loads(question.to_json())
+        _logger.info(f"obj: {json.dumps(obj, indent=2, sort_keys=True)}")
+        MongoClient().alex_flashcards.results.insert_one(obj)
+
+@flashcards.command()
 @click.pass_context
 def show_cards(ctx):
     """
@@ -111,81 +215,13 @@ def show_cards(ctx):
     #print(f"deck_size: {deck_size}")
     print(pd.DataFrame(ctx.obj["cards"]))
 
-
-@flashcards.command()
-@click.option("--deck_size", type=int, default=5, envvar="DECK_SIZE")
-@click.option("--deck_index", type=int, default=-1, envvar="DECK_INDEX")
-@click.option("--full/--no-full", default=False)
-@click.option("--sort/--no-sort", default=False)
-@click.pass_context
-def show_score(ctx, deck_index, deck_size, full, sort):
-    _logger = logging.getLogger("show_score")
-    cards = ctx.obj["cards"]
-    _logger.info(f"{int(len(cards)/deck_size)+1} decks")
-    assert deck_index >= 0
-
-    deck = cards[deck_size*deck_index:deck_size*(deck_index+1)]
-    deck = [{k: v for k, v in c.items() if k not in ["tags"]}
-            for c in deck]
-    assert(len(deck) > 0)
-    _logger.info(f"deck:\n{pd.DataFrame(deck)}")
-
-    deck_df = _get_deck_with_score(deck)
-
-    if not full:
-        deck_df = deck_df.drop(columns=["back", "front"])
-    else:
-        for i in range(deck_df["back"].apply(len).max()):
-            deck_df[f"back[{i}]"] = deck_df["back"].apply(lambda back:None if len(back)<=i else back[i])
-        deck_df = deck_df.drop(columns=["back"])    
-    if sort:
-        deck_df = deck_df.sort_values(by="score", ascending=False)
-
-    print(deck_df.to_string(justify="left",col_space=20))
-    print(f"average score: {deck_df['score'].mean()*100:05.2f}%")
-
-
-@flashcards.command()
-@click.option("--deck_size", type=int, default=5, envvar="DECK_SIZE")
-@click.option("--deck_index", type=int, default=-1, envvar="DECK_INDEX")
-@click.pass_context
-def test(ctx, deck_index, deck_size):
-    _logger = logging.getLogger("test")
-    cards = ctx.obj["cards"]
-    _logger.info(f"{int(len(cards)/deck_size)+1} decks")
-    assert deck_index >= 0
-
-    deck = cards[deck_size*deck_index:deck_size*(deck_index+1)]
-    deck = [{k: v for k, v in c.items() if k not in ["tags"]}
-            for c in deck]
-    assert(len(deck) > 0)
-    _logger.info(f"deck:\n{pd.DataFrame(deck)}")
-    question_i = 0
-    while True:
-        question_i += 1
-        _d = _get_random_question(deck)
-        question = get_question(
-            _d["question_type"], **{k: v for k, v in _d.items() if k != "question_type"})
-        print(f"question #{question_i}: \n{question.get_question_text()}")
-        while True:
-            res, msg = question.grade(input("answer> "))
-            if msg is not None:
-                print(f"({msg})")
-            if res is not None:
-                break
-        print(f"res: {res}")
-        obj = json.loads(question.to_json())
-        _logger.info(f"obj: {json.dumps(obj, indent=2, sort_keys=True)}")
-        MongoClient().alex_flashcards.results.insert_one(obj)
-
-
-@flashcards.command()
-@click.argument("front")
-@click.option("--back", multiple=True)
-@click.pass_context
+@ flashcards.command()
+@ click.argument("front")
+@ click.option("--back", multiple=True)
+@ click.pass_context
 def add_item(ctx, **kwargs):
-    coll = ctx.obj["coll"]
-    obj = {"tags": ctx.obj["tags"], **kwargs}
+    coll=ctx.obj["coll"]
+    obj={"tags": ctx.obj["tags"], **kwargs}
     print(f"inserting {obj}")
     coll.insert_one(obj)
 
