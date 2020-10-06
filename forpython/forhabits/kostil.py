764b8f2e3ee0f55607ae0bac18e926eee7e6a825
Alex Leontiev <nailbiter@dtws-work.in>
2020-10-06 21:33:45 +0900

    stash

--- forpython/forhabits/kostil.py
+++ forpython/forhabits/kostil.py
@@ -19,8 +19,8 @@ _WEEKDAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 # procedures
 
 
-def task_predicate(task, date, **kwargs):
-    _logger = logging.getLogger("task_predicate")
+def _task_predicate(task, date, **kwargs):
+    _logger = logging.getLogger("_task_predicate")
     _logger.info(f"date: {date}")
     _logger.info(f"task: {task}")
     if not task["enabled"]:
@@ -46,10 +46,12 @@ def cli(debug=False):
 @cli.command()
 @click.argument("task")
 @click.option("--dry_run/--no-dry_run", default=False)
-def done(task,dry_run):
+def done(task, dry_run):
     tasks_df = _get_tasks()
-    task_obj = tasks_df[[name==task for name in tasks_df["name"]]].to_dict(orient="records")[0]
-    obj = {"task_name": task, "task_id": task_obj["_id"], "datetime": datetime.now()}
+    task_obj = tasks_df[[name == task for name in tasks_df["name"]]].to_dict(
+        orient="records")[0]
+    obj = {"task_name": task,
+           "task_id": task_obj["_id"], "datetime": datetime.now()}
     print(f"done obj: {obj}")
     if not dry_run:
         MongoClient().habits.habits_done.insert_one(obj)
@@ -73,7 +75,7 @@ def add(date, mongopass, dry_run, debug=False, only_permanent_habits=False):
         _debug = set()
         logger = logging.getLogger("cli")
         tasks = [{k: v for k, v in task.items() if k not in _NOT_SHOW_FIELDS}
-                 for task in task_data_coll.find() if task_predicate(task, d, debug=_debug, only_permanent_habits=only_permanent_habits)]
+                 for task in task_data_coll.find() if _task_predicate(task, d, debug=_debug, only_permanent_habits=only_permanent_habits)]
 
         logger.info(f"\n{DataFrame(tasks)}")
         logger.info(f"d: {d.strftime('%Y-%m-%d')}")
@@ -86,23 +88,26 @@ def add(date, mongopass, dry_run, debug=False, only_permanent_habits=False):
     print(res)
 
     if not dry_run:
-        # TODO
-        pass
+        MongoClient().habits.habits.insert_many(res.to_dict(orient="records"))
+
 
 def _get_tasks():
     tasks_df = DataFrame(MongoClient().habits.habits.find()).set_index("_id")
-    habits_done_df = DataFrame(MongoClient().habits.habits_done.find()).set_index("task_id")
+    habits_done_df = DataFrame(
+        MongoClient().habits.habits_done.find()).set_index("task_id")
     tasks_df = tasks_df.join(habits_done_df)
     tasks_df = tasks_df[[isna(datetime) for datetime in tasks_df["datetime"]]]
     tasks_df = tasks_df.drop(columns=["_id"]).reset_index()
     return tasks_df
 
+
 @cli.command()
 def list():
     tasks_df = _get_tasks()
-    tasks_df = tasks_df.drop(columns=["_id","datetime","task_name", "creation date"])
+    tasks_df = tasks_df.drop(
+        columns=["_id", "datetime", "task_name", "creation date"])
     tasks_df["count"] = 1
-    df = tasks_df.groupby("name").aggregate({"count":np.sum})
+    df = tasks_df.groupby("name").aggregate({"count": np.sum})
 
     print(df.to_string())
     print(f"sum: {sum(df['count'])}")
