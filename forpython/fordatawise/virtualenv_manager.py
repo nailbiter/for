90d933a179699e831bd16bfef4ad6052cf4f3b58
Alex Leontiev <nailbiter@dtws-work.in>
2020-11-03 12:28:10 +0900

    virtualenv_manager.py

--- forpython/fordatawise/virtualenv_manager.py
+++ forpython/fordatawise/virtualenv_manager.py
@@ -26,24 +26,25 @@ from re import match
 from subprocess import getoutput
 
 
-
 @click.group()
-@click.option("--debug/--no-debug",default=True)
+@click.option("--debug/--no-debug", default=True)
 def virtualenv_manager(debug):
     if debug:
         logging.basicConfig(level=logging.INFO)
+
 
 def _system(cmd):
     _logger = logging.getLogger("_system")
     _logger.info(f"> {cmd}")
     system(cmd)
 
+
 @virtualenv_manager.command()
 def add_kernel():
-    pwd=getoutput("pwd")
-    pwd = pwd.replace("/",".").strip(".")
+    pwd = getoutput("pwd")
+    pwd = pwd.replace("/", ".").strip(".")
     # Kernel names can only contain ASCII letters and numbers and these separators: - . _ (hyphen, period, and underscore).
-    assert match(r"^[a-zA-Z0-9._-]+$",pwd) is not None
+    assert match(r"^[a-zA-Z0-9._-]+$", pwd) is not None
     print(f"pwd: {pwd}")
     _system(f"pip install ipykernel")
     _system(f"ipython kernel install --user --name={pwd}")
@@ -51,25 +52,29 @@ def add_kernel():
 
 @virtualenv_manager.command()
 @click.argument("package_name")
-@click.option("--save/--no-save",default=True)
-def install(package_name,save):
+@click.option("--save/--no-save", default=True)
+@click.option("-v","--version")
+def install(package_name, save, version):
     pip3 = "./venv/bin/pip3"
-    #if match(r"https://github.com/[^/]+/[^/]+/",package_name) is not None:
+    # if match(r"https://github.com/[^/]+/[^/]+/",package_name) is not None:
     if package_name.startswith("https://github.com"):
         _MODE = "github"
     else:
         _MODE = "usual"
-    if _MODE=="github":
+    if _MODE == "github":
         package_name = package_name[len("https:"):]
         package_name = f"git+https:{package_name}"
+    if version is not None:
+        package_name = f"{package_name}@{version}"
     _system(f"{pip3} install {package_name}")
     if save:
         _system(f"touch requirements.txt")
-        if _MODE=="github":
+        if _MODE == "github":
             _system(f"echo \"{package_name}\" >> requirements.txt")
         else:
-            #FIXME: this can be done more robustly
-            _system(f"{pip3} freeze | grep -i \"{package_name}\" >> requirements.txt")
+            # FIXME: this can be done more robustly
+            _system(
+                f"{pip3} freeze | grep -i \"{package_name}\" >> requirements.txt")
         print(f"added \"{package_name}\"")
 
 
