23a87c11683501b9ecf198d3668080c0fede4186
Alex Leontiev <nailbiter@dtws-work.in>
2020-11-24 20:18:30 +0900

    bq.py

--- forpython/fordatawise/bq.py
+++ forpython/fordatawise/bq.py
@@ -20,11 +20,41 @@ ORGANIZATION:
 ==============================================================================="""
 
 import click
+from google.cloud import bigquery
+from pymongo import MongoClient
+from datetime import datetime
+import logging
 
-@click.command()
-@click.argument("fn",type=click.Path())
-def bq(fn):
+
+_SAVE_RESULTS_MAX_RECORD_NUM = 1000
+
+@click.group()
+def bq():
     pass
+
+@bq.command()
+@click.argument("fn",type=click.Path())
+@click.option("-p","--project")
+@click.option("-o","--oformat",type=click.Choice(["csv"]),default="csv")
+def query(fn,project,oformat):
+    bq_client_kwargs = {}
+    if project is not None:
+        bq_client_kwargs["project"] = project
+    bq_client = bigquery.Client(**bq_client_kwargs)
+    with open(fn) as f:
+        sql = f.read()
+    bytes_processed = bq_client.query(sql,job_config=bigquery.QueryJobConfig(dry_run=True,use_cache=False)).total_bytes_processed
+    df = bq_client.query(sql).to_dataframe(progress_bar_type="tqdm")
+
+    if oformat=="csv":
+        print(df.to_csv(index=None))
+    else:
+        raise NotImplementedError
+
+    mongo_client = MongoClient()
+    if len(df)>_SAVE_RESULTS_MAX_RECORD_NUM:
+        logging.warning(f"response has more records than we can store ({len(df)}>{_SAVE_RESULTS_MAX_RECORD_NUM}) => truncating")
+    mongo_client.datawise.bq_query_log.insert_one({"sql":sql,"date":datetime.now(),"res":df.to_dict(orient="records")[:_SAVE_RESULTS_MAX_RECORD_NUM],"bytes_processed":bytes_processed})
 
 if __name__=="__main__":
     bq()
