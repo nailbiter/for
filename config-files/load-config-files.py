6ae39db5c65b256970e77028e8116bd411b00d30
Alex Leontiev <nailbiter@dtws-work.in>
2020-11-26 13:29:37 +0900

    misc

---
+++ config-files/load-config-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: /Users/nailbiter/for/config-files/load-config-files.py
+
+       USAGE: .//Users/nailbiter/for/config-files/load-config-files.py
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2020-11-26T13:03:45.850841
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+
+@click.command()
+def load_config_files():
+    pass
+
+if __name__=="__main__":
+    load_config_files()
--- forpython/fordatawise/bq_log.py
+++ forpython/fordatawise/bq_log.py
@@ -29,12 +29,20 @@ from pymongo import MongoClient
 import os
 
 
+_PROJECTS = [
+    "olm-1-datawarehouse-dev", 
+    "olm-limited-access",
+    "olm-datamart-prd"
+]
 @click.command()
 @click.option("--start-date", type=click.DateTime())
-@click.option("--slack/--no-slack", default=True)
+@click.option("--slack/--no-slack", default=False)
 @click.option("--slack-webhook")
 @click.option("--bigquery-credentials", type=click.Path(), default="/Users/nailbiter/.config/gcloud/legacy_credentials/dtws_oleksii_leontiev@oaklawn.co.jp/adc.json")
-def bq_log(start_date, slack, slack_webhook, bigquery_credentials):
+@click.option("--project", type=click.Choice(_PROJECTS), multiple=True)
+def bq_log(start_date, slack, slack_webhook, bigquery_credentials, project):
+    if len(project)==0:
+        project = _PROJECTS
     if start_date is None:
         start_date = datetime.now()
     if slack_webhook is None:
@@ -50,9 +58,9 @@ def bq_log(start_date, slack, slack_webhook, bigquery_credentials):
                 j.total_bytes_processed, "created": j.created+timedelta(hours=9), "project": j.project}
             for j
             # in client.list_jobs(project="olm-limited-access", state_filter="done", max_results=100)
-            in client.list_jobs(project=project, state_filter="done", min_creation_time=datetime(start_date.year, start_date.month, start_date.day))
+            in client.list_jobs(project=project_, state_filter="done", min_creation_time=datetime(start_date.year, start_date.month, start_date.day))
         ])
-        for project in ["olm-1-datawarehouse-dev", "olm-limited-access"]
+        for project_ in project
     ])
     print(df.to_csv(index=None))
     df = df.fillna(0)
