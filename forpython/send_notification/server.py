9bb2958fb7e35a4801eca68500476f07d07701e2
Alex Leontiev <nailbiter@dtws-work.in>
2020-11-27 20:15:26 +0900

    excise

---
+++ forpython/send_notification/server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: /Users/nailbiter/for/forpython/send_notification/server.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2020-11-27T19:49:01.715259
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+
+@click.command()
+def server():
+    pass
+
+if __name__=="__main__":
+    server()
