2109bd2347c08791e2a3908ef5d5cffe5673e755
Alex Leontiev <nailbiter@dtws-work.in>
2020-11-30 23:04:46 +0900

    send_notification:click:fixup

--- forpython/send_notification/client.py
+++ forpython/send_notification/client.py
@@ -23,16 +23,22 @@ import click
 import os
 from urllib import parse
 from datetime import datetime, timedelta
+from _send_notification import system
+import logging
 
 
 @click.command()
-@click.option("-d","--delay-min",default=0)
+@click.option("-d", "--delay-min", default=0)
+@click.option("--debug/--no-debug", default=True)
 @click.argument("message")
-def client(delay_min,message):
+def client(delay_min, message, debug):
+    if debug:
+        logging.basicConfig(level=logging.INFO)
     dt = datetime.now() + timedelta(minutes=delay_min)
     message = parse.quote(message)
     url = f"localhost:5000/new_timer/{dt.strftime('%Y%m%d%H%M%S')}/{message}/slack"
-    os.system(f"curl \"{url}\"")
+    system(f"curl \"{url}\"")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     client()
