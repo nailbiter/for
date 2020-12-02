ccefc664590031d48d066f126abc946fae3ab1eb
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-02 19:29:56 +0900

    kostil.py

--- forpython/fordatawise/kostil.py
+++ forpython/fordatawise/kostil.py
@@ -7,6 +7,9 @@ import click
 from itertools import product
 from os.path import splitext, isfile
 import json
+import uuid
+import subprocess
+import os
 
 
 # global const's
@@ -16,7 +19,7 @@ _KEY_VALUES = {
     "anken": ['データ収集/天気', 'アルゴリズム改善', 'その他', '推定アルゴリズム/性別推定', 'Datawise Retail', 'covid19'],
     "task": ['その他', '分析・検討・調査', 'レビュー', '資料作成', '会議', 'コーディング']
 }
-for k in ["anken","task"]:
+for k in ["anken", "task"]:
     if isfile(f".{k}.json"):
         with open(f".{k}.json") as f:
             _KEY_VALUES[k] = json.load(f)[k]
@@ -26,18 +29,36 @@ for k in ["anken","task"]:
 def cli():
     pass
 
+
 @cli.command()
-@click.argument("fn",type=click.Path(exists=True))
+@click.argument("fn", type=click.Path(exists=True))
 def update(fn):
-    assert splitext(fn)[1]==".csv"
+    assert splitext(fn)[1] == ".csv"
     df = pd.read_csv(fn)
-    for k,v in {"anken":"案件","task":"タスク"}.items():
+    for k, v in {"anken": "案件", "task": "タスク"}.items():
         _x = sorted([x for x in df[v].unique() if not pd.isna(x)])
         print(f"{k}: {_x}")
-        with open(f".{k}.json","w") as f:
-            json.dump({k:_x},f,ensure_ascii=False,indent=2)
+        with open(f".{k}.json", "w") as f:
+            json.dump({k: _x}, f, ensure_ascii=False, indent=2)
 
-#@click.option("--anken", type=click.IntRange(0, len(_KEY_VALUES['anken'])-1), help=f"\n{pd.DataFrame({'anken':_KEY_VALUES['anken']})}")
+
+def _get_fzf_selection(key):
+    values = _KEY_VALUES[key]
+    #FIXME: do this in-memory
+    fn = f"/tmp/{uuid.uuid4()}.txt"
+    with open(fn,"w") as f:
+        for i,value in enumerate(values):
+            f.write(f"{i}. {value}\n")
+
+    fn2 = f"/tmp/{uuid.uuid4()}.txt"
+    os.system(f"cat {fn} | fzf --no-sort | sponge {fn2}")
+    with open(fn2) as f:
+        res = f.read()
+    res = res.strip()
+    res = res.split(".")[0]
+    return int(res)
+
+
 @cli.command()
 @click.argument("task_id", type=str)
 @click.option("--anken", type=click.IntRange(0, len(_KEY_VALUES['anken'])-1), help="; ".join([f"{i}. {n}" for i, n in enumerate(_KEY_VALUES["anken"])]))
@@ -45,8 +66,14 @@ def update(fn):
 @click.option("--test/--no-test", default=False)
 def kostil(task_id, test, **kwargs):
     print(f"hi: {task_id}")
-    assert([k for k, v in kwargs.items() if v is not None]
-           ), "at least one of `anken` or `task` should be given"
+#    assert([k for k, v in kwargs.items() if v is not None]
+#           ), "at least one of `anken` or `task` should be given"
+    for k in ["anken", "task"]:
+        if kwargs[k] is None:
+            res = _get_fzf_selection(k)
+#            print(f"res: \"{res}\"")
+#            exit(1)
+            kwargs[k] = res
 
     local_client = MongoClient('localhost', 27017)
     coll = local_client.admin.passwords
