1fd1c818b745b90d7d4cee681b6b0ae6c67db47c
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-04 19:00:10 +0900

    eta.py

---
+++ forpython/miscutils/eta.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: miscutils/eta.py
+
+       USAGE: ./miscutils/eta.py
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2020-12-03T14:25:16.801468
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import pandas as pd
+
+
+@click.group()
+def eta():
+    pass
+
+
+def _get_mongo_client():
+    return MongoClient()
+
+
+@eta.command()
+@click.argument("name")
+@click.option("-m", "--max-count", type=int, default=-1)
+def create_counter(name, max_count):
+    # FIXME: check that name is unique
+    _get_mongo_client().eta.timers.insert_one(
+        {"name": name, "max_count": max_count, "date": datetime.now()})
+
+
+@eta.command()
+@click.argument("name")
+def increment_counter(name):
+    timer = _get_mongo_client().eta.timers.find_one({"name": name})
+    _get_mongo_client().eta.timer_progress.insert_one(
+        {"name": name, "date": datetime.now()})
+    if timer["max_count"] > 0:
+        _show_counter(name)
+
+
+def _show_counter(name, max_count=-1):
+    timer = _get_mongo_client().eta.timers.find_one({"name": name})
+    df = pd.DataFrame(
+        _get_mongo_client().eta.timer_progress.find({"name": name}))
+    print(df)
+    assert max_count > 0 or timer["max_count"] > 0
+    if timer["max_count"] > 0:
+        max_count = timer["max_count"]
+    eta_ = timer["date"] + (max(df.date)-timer["date"]) / \
+        (len(df)) * max_count
+    print(f"eta: {eta_.isoformat()}")
+    print(f"{len(df)}/{max_count}={len(df)/max_count*100:4.2f}")
+    print(f"average length of iteration: {(max(df.date)-timer['date']) / (len(df))}")
+
+
+@eta.command()
+@click.argument("name")
+@click.option("-m", "--max-count", type=int, default=-1)
+def show_counter(name, max_count):
+    _show_counter(name, max_count=max_count)
+
+
+if __name__ == "__main__":
+    eta()
