48ace3359bd14503139159614f06da5e07f77065
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-05 21:08:06 +0900

    bookmarks

--- forpython/miscutils/eta.py
+++ forpython/miscutils/eta.py
@@ -57,6 +57,32 @@ def increment_counter(name):
         _show_counter(name)
 
 
+@eta.command()
+@click.argument("name")
+@click.option("-t","--time",type=click.DateTime())
+def stop_timer(name,time):
+    if time is None:
+        time = datetime.now()
+    timer = _get_mongo_client().eta.timers.find_one({"name": name})
+    _get_mongo_client().eta.timer_time.insert_one(
+        {"name": name, "date": time, "is_stop": True})
+    if timer["max_count"] > 0:
+        _show_counter(name)
+
+
+@eta.command()
+@click.argument("name")
+@click.option("-t","--time",type=click.DateTime())
+def resume_timer(name,time):
+    if time is None:
+        time = datetime.now()
+    timer = _get_mongo_client().eta.timers.find_one({"name": name})
+    _get_mongo_client().eta.timer_time.insert_one(
+        {"name": name, "date": time, "is_stop": False})
+    if timer["max_count"] > 0:
+        _show_counter(name)
+
+
 def _add_logger(f):
     logger = logging.getLogger(f.__name__)
 
@@ -68,14 +94,27 @@ def _add_logger(f):
 @_add_logger
 def _show_counter(name, logger, max_count=-1):
     timer = _get_mongo_client().eta.timers.find_one({"name": name})
+    client = _get_mongo_client()
     df = pd.DataFrame(
-        _get_mongo_client().eta.timer_progress.find({"name": name}))
+        client.eta.timer_progress.find({"name": name}))
     logger.info(df)
     assert max_count > 0 or timer["max_count"] > 0
     if timer["max_count"] > 0:
         max_count = timer["max_count"]
-    eta_ = timer["date"] + (max(df.date)-timer["date"]) / \
-        (len(df)) * max_count
+    timer_duration = max(df.date)-timer["date"]
+    stop_starts = pd.DataFrame(client.eta.timer_time.find({"name": name}))
+    stop_starts = stop_starts.sort_values(by="date")
+    stop_starts = stop_starts.to_dict(orient="records")
+    excessive_time = timedelta(0)
+    if len(stop_starts) > 0:
+        if len(stop_starts) % 2 == 1:
+            stop_starts.append({"date": datetime.now(), "is_stop": False})
+        for i in range(0, len(stop_starts), 2):
+            stop, restart = stop_starts[i:i+2]
+            assert stop["is_stop"] and not restart["is_stop"]
+            excessive_time += (restart["date"]-stop["date"])
+
+    eta_ = timer["date"] + excessive_time + (timer_duration-excessive_time) / len(df) * max_count
     s = ""
     for i in range(len(df)):
         s += "*"
--- forpython/send_notification/resources/executor.py
+++ forpython/send_notification/resources/executor.py
@@ -41,6 +41,7 @@ def _system(cmd,logger):
 _LOG_DIR = "/tmp/executor_log_17a6097921e1b45c"
 os.makedirs(_LOG_DIR, exist_ok=True)
 random_fn = "".join(random.choices(population=list(string.ascii_letters+string.digits+"_"),k=32))
+random_fn = f"{random_fn}.log.txt"
 logging.basicConfig(filename=path.join(_LOG_DIR,random_fn),
                     encoding='utf-8', level=logging.DEBUG)
 logging.info("here")
