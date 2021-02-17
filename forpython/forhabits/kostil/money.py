0c823a07f5048bc4ef4478a5d4211675ef770f5f
Alex Leontiev <nailbiter@dtws-work.in>
2021-02-17 15:56:57 +0900

    stash

--- forpython/forhabits/kostil/money.py
+++ forpython/forhabits/kostil/money.py
@@ -25,7 +25,11 @@ from _common import get_remote_mongo_client
 from pytz import timezone
 from bson.codec_options import CodecOptions
 import pandas as pd
+import numpy as np
 import pymongo
+import logging
+import json
+
 
 def _get_coll(mongo_pass):
     client = get_remote_mongo_client(mongo_pass)
@@ -33,16 +37,59 @@ def _get_coll(mongo_pass):
         codec_options=CodecOptions(tz_aware=True, tzinfo=timezone('Asia/Tokyo')))
     return coll
 
+
+def _add_logger(f):
+    logger = logging.getLogger(f.__name__)
+
+    def _f(*args, **kwargs):
+        return f(*args, logger=logger, **kwargs)
+    _f.__name__ = f.__name__
+    return _f
+
 @click.command()
-@click.option("-d","--day",type=click.DateTime(["%Y-%m-%d"]))
+@click.option("-d", "--day", type=click.DateTime(["%Y-%m-%d"]))
+@click.option("-m", "--mode", type=click.Choice(["daily", "monthly"]), default="daily")
 @click.option("--mongo_pass", envvar="MONGO_PASS", required=True)
-def money(day,mongo_pass):
+@click.option("--debug/--no-debug",default=False)
+@click.option("--monthly-regular-payments-file-name",type=click.Path(),default=".monthly_regular_payments.json")
+@_add_logger
+def money(day, mongo_pass, mode, debug,monthly_regular_payments_file_name,logger=None):
+    if debug:
+        logging.basicConfig(level=logging.INFO)
     if day is None:
         day = datetime.now()
     coll = _get_coll(mongo_pass)
-    money_df = pd.DataFrame(
-            coll.find({"$and":[{"date":{"$gte":datetime(day.year,day.month,day.day)}},{"date":{"$lt":datetime(day.year,day.month,day.day)+timedelta(days=1)}}]},sort=[("date", pymongo.DESCENDING)]))
-    print(money_df.to_string())
+    if mode == "daily":
+        money_df = pd.DataFrame(
+            coll.find({"$and": [{"date": {"$gte": datetime(day.year, day.month, day.day)}}, {"date": {"$lt": datetime(day.year, day.month, day.day)+timedelta(days=1)}}]}, sort=[("date", pymongo.DESCENDING)]))
+        click.echo(money_df.to_string())
+    elif mode == "monthly":
+        next_month = [day.year, day.month]
+        next_month[1] += 1
+        if next_month[1] >= 12:
+            next_month[1] = next_month[1] % 12
+            next_month[0] += 1
+        money_df = pd.DataFrame(
+            coll.find({"$and": [{"date": {"$gte": datetime(day.year, day.month, 1)}}, {"date": {"$lt": datetime(*next_month, 1)+timedelta(days=1)}}]}, sort=[("date", pymongo.DESCENDING)]))
+        money_df = money_df[[d.month==day.month and d.year==d.year for d in money_df["date"]]]
+        with open(monthly_regular_payments_file_name) as f:
+            monthly_regular_payments = json.load(f)
+        for r in monthly_regular_payments:
+            money_df = money_df.append({
+                **{"tags":[]},
+                **{k:v for k,v in r.items() if k!="date"},
+                "date":datetime(day.year,day.month,**r["date"]),
+            },ignore_index=True)
+        logger.info(money_df.to_csv())
 
-if __name__=="__main__":
+        money_df = money_df.groupby("category").agg({"amount":np.sum})
+        money_df = money_df.reset_index()
+        money_df = money_df.append({"category":"_total","amount":money_df.amount.sum()},ignore_index=True)
+        money_df = money_df.set_index("category")
+        click.echo(money_df.to_string())
+    else:
+        raise NotImplementedError(f"unknown mode \"{mode}\"")
+
+
+if __name__ == "__main__":
     money()
