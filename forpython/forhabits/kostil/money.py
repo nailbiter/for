a4f33dd6d380531af3e9eb2d4e73ec375e128df6
Alex Leontiev <nailbiter@dtws-work.in>
2021-03-09 17:22:13 +0900

    stash

--- forpython/forhabits/kostil/money.py
+++ forpython/forhabits/kostil/money.py
@@ -29,6 +29,7 @@ import numpy as np
 import pymongo
 import logging
 import json
+import requests
 
 
 def _get_coll(mongo_pass):
@@ -52,8 +53,9 @@ def _add_logger(f):
 @click.option("--mongo_pass", envvar="MONGO_PASS", required=True)
 @click.option("--debug/--no-debug",default=False)
 @click.option("--monthly-regular-payments-file-name",type=click.Path(),default=".monthly_regular_payments.json")
+@click.option("--monthly-channel-webhook",envvar="MONTHLY_CHANNEL_WEBHOOK")
 @_add_logger
-def money(day, mongo_pass, mode, debug,monthly_regular_payments_file_name,logger=None):
+def money(day, mongo_pass, mode, debug,monthly_regular_payments_file_name,monthly_channel_webhook,logger=None):
     if debug:
         logging.basicConfig(level=logging.INFO)
     if day is None:
@@ -64,6 +66,7 @@ def money(day, mongo_pass, mode, debug,monthly_regular_payments_file_name,logger
             coll.find({"$and": [{"date": {"$gte": datetime(day.year, day.month, day.day)}}, {"date": {"$lt": datetime(day.year, day.month, day.day)+timedelta(days=1)}}]}, sort=[("date", pymongo.DESCENDING)]))
         click.echo(money_df.to_string())
     elif mode == "monthly":
+        assert monthly_channel_webhook is not None
         next_month = [day.year, day.month]
         next_month[1] += 1
         if next_month[1] >= 12:
@@ -87,6 +90,12 @@ def money(day, mongo_pass, mode, debug,monthly_regular_payments_file_name,logger
         money_df = money_df.append({"category":"_total","amount":money_df.amount.sum()},ignore_index=True)
         money_df = money_df.set_index("category")
         click.echo(money_df.to_string())
+        requests.post(monthly_channel_webhook, json.dumps({
+            "text": f"""```{day.strftime("%Y-%m")}\n{money_df.to_string()}```"""
+        }),
+            headers={
+                "Content-type": "application/json"
+        })
     else:
         raise NotImplementedError(f"unknown mode \"{mode}\"")
 
