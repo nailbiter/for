ed331a01196ddd588db5ea787826d853b275800f
Alex Leontiev <nailbiter@dtws-work.in>
2021-03-19 23:23:57 +0900

    stash

---
+++ forpython/fordatawise/non-reusable/print-hourly-work-time.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: /Users/nailbiter/for/forpython/fordatawise/non-reusable/print-hourly-work-time.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-03-19T22:23:38.665741
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+
+@click.command()
+def print_hourly_work_time():
+    pass
+
+if __name__=="__main__":
+    print_hourly_work_time()
