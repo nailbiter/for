61497718d0ad5ece11275dc4fc5d3103e00e2983
Alex Leontiev <nailbiter@dtws-work.in>
2021-03-21 16:54:45 +0900

    stash

--- forpython/_new_file/new_file/click.jinja.py
+++ forpython/_new_file/new_file/click.jinja.py
@@ -19,6 +19,7 @@ ORGANIZATION: {{organization}}
 
 ==============================================================================="""
 {%-set basename = os.path.splitext(os.path.split(filename)[-1])[0]|replace('-','_')%}
+{%-set basename = ("s_"+basename) if basename[0] in "0123456789" else basename-%}
 
 import click
 
