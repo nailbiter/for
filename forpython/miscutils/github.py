ecb9cd71ab7b386881cd0f5d2044385238af8fa5
Alex Leontiev <nailbiter@dtws-work.in>
2021-06-15 07:55:55 +0900

    miscutils: github
    auto-commit functionality

--- forpython/miscutils/github.py
+++ forpython/miscutils/github.py
@@ -28,6 +28,7 @@ from jinja2 import Template
 import logging
 import re
 import glob
+from git import Repo
 
 
 def _add_logger(f):
@@ -58,14 +59,38 @@ def github(debug):
 def branch():
     click.echo(_get_branch_name())
 
+
 def _cleanup_remote_git_url(remote_git_url):
     if remote_git_url.endswith(".git"):
         remote_git_url = remote_git_url[:-4]
-    m = re.match("(https://)[^/]+@(github.com/.*)",remote_git_url)
+    m = re.match("(https://)[^/]+@(github.com/.*)", remote_git_url)
     if m is not None:
         remote_git_url = m.group(1)+m.group(2)
 #    https://nailbiter91@github.com/dtws/traffic-volume
     return remote_git_url
+
+
+def _get_head_sha(path=None):
+    """
+    return hex, is_no_modifications_done
+    """
+    if path is None:
+        _path = "."
+        # FIXME: this probably can be done better
+        while True:
+            try:
+                repo = Repo(_path)
+                break
+            except Exception:
+                _path = path.join(_path, "..")
+    else:
+        repo = Repo(path)
+    assert not repo.bare
+    head_commit = repo.head.commit
+#    assert(not head_commit.diff(None)
+#           ), "should be no changes on tree (do `git commit -a`)"
+    return head_commit.hexsha, not head_commit.diff(None)
+
 
 @github.command(name="open")
 @click.option("-f", "--file-name", type=click.Path(), default=".", multiple=True)
@@ -74,23 +99,33 @@ def _cleanup_remote_git_url(remote_git_url):
 @click.option("--open-url/--no-open-url", default=True)
 @click.option("--commit")
 @click.option("--head", type=int)
+@click.option("--auto-commit", envvar="GITHUB__OPEN_URL__AUTOCOMMIT")
 @_add_logger
-def open_url(file_name, freeze_commit, open_url, branch, head, commit, logger=None):
+def open_url(file_name, freeze_commit, open_url, branch, head, commit, auto_commit, logger=None):
     _file_name = []
     for fn in file_name:
         if "*" in fn:
-            _file_name.extend(glob.glob(fn,recursive=True))
+            _file_name.extend(glob.glob(fn, recursive=True))
         else:
             _file_name.append(fn)
-    file_name = _file_name        
+    file_name = _file_name
 
-    #TODO: add warning if commit is not pushed
 #    git_dir = "."
     git_dir = path.split(file_name[0])[0]
     while not path.isdir(path.join(git_dir, ".git")):
         git_dir = path.join(git_dir, "..")
+
+    # TODO: add warning if commit is not pushed
+    _, is_no_modifications_done = _get_head_sha(path=git_dir)
+    if auto_commit is not None:
+        if not is_no_modifications_done:
+            os.system(f"git commit -a -m '{auto_commit}' && git push")
+    else:
+        logger.warning(f"there are local modifications!")
+
     #click.echo(f"git_dir: {git_dir}")
-    remote_git_url = subprocess.getoutput(f"cd {git_dir} && git remote get-url origin")
+    remote_git_url = subprocess.getoutput(
+        f"cd {git_dir} && git remote get-url origin")
     remote_git_url = _cleanup_remote_git_url(remote_git_url)
     #click.echo(f"remote_git_url: {remote_git_url}")
     if branch is None:
