cf99a5b11d74b3297f13af07ae75bfdd4962d8a3
Oleksii Leontiev <alozz1991@gmail.com>
2022-08-29 21:54:45 +0900

    stash

--- forpython/miscutils/random-fn.py
+++ forpython/miscutils/random-fn.py
@@ -26,27 +26,31 @@ import os
 import sqlite3
 import pandas as pd
 
+
 @click.command()
-@click.argument("ext",default="")
-@click.option("--database-fn",type=click.Path(),default=path.join(path.split(__file__)[0],".random_fn.db"))
-@click.option("--read/--no-read",default=False)
-def random_fn(ext,database_fn,read):
-#    click.echo(f"ext: \"{ext}\"")
-    if ext=="":
+@click.argument("ext", default="")
+@click.option("--database-fn", type=click.Path(), default=path.join(path.split(__file__)[0], ".random_fn.db"))
+@click.option("--read/--no-read", "-r/ ", default=False)
+@click.option("-d", "--tmp-dir", type=click.Path(), default="/tmp")
+def random_fn(ext, database_fn, read, tmp_dir):
+    #    click.echo(f"ext: \"{ext}\"")
+    if ext == "":
         ext = None
     conn = sqlite3.connect(database_fn)
     if not read:
-        fn = f"/tmp/{str(uuid.uuid4()).replace('-','_')}"
+        fn = f"{tmp_dir}/{str(uuid.uuid4()).replace('-','_')}"
         if ext is not None:
             assert ext.startswith(".")
             fn += ext
         else:
-            os.makedirs(fn,exist_ok=True)
-        pd.DataFrame({"filename":[fn]}).to_sql("filenames",conn,if_exists="append",index=None)
+            os.makedirs(fn, exist_ok=True)
+        pd.DataFrame({"filename": [fn]}).to_sql(
+            "filenames", conn, if_exists="append", index=None)
         click.echo(fn)
     else:
         df = pd.read_sql_query('SELECT * FROM filenames', conn)
         click.echo(list(df["filename"])[-1])
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     random_fn()
