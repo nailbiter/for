14b30a8911d6fcb6bd4c25d3a3aa1e861c4f4c7d
Oleksii Leontiev <alozz1991@gmail.com>
2022-12-31 10:00:05 +0900

    leo-docker

---
+++ forpython/miscutils/_common.py
@@ -0,0 +1,21 @@
+"""===============================================================================
+
+        FILE: _common.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2022-12-31T09:59:13.639716
+    REVISION: ---
+
+==============================================================================="""
+
+def request
