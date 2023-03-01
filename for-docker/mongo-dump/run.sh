035e915a813f24579b3bf4954e0bff45bd5c42a9
Alex Leontiev <alozz1991@gmail.com>
2023-03-01 18:56:48 +0900

    mongodump

--- for-docker/mongo-dump/run.sh
+++ for-docker/mongo-dump/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -it --env-file=.env -v `pwd`/output/:/output:rw nailbiter/mongo-dump
+/snap/bin/docker run -it --env-file=.env -v `pwd`/output/:/output:rw nailbiter/mongo-dump
 #docker run -v `pwd`/notebooks:/notebooks:ro -v `pwd`/pdfs:/pdfs:rw -it nailbiter/jupyter-latex-pdf:latest
