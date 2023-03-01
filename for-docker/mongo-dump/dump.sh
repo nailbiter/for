82eaa221696a387c08b170ea1311cda46f2c22e3
Oleksii Leontiev <alozz1991@gmail.com>
2023-03-01 18:10:41 +0900

    stash

--- for-docker/mongo-dump/dump.sh
+++ for-docker/mongo-dump/dump.sh
@@ -4,4 +4,6 @@ DATE=`date "+%Y-%m-%d"`
 #echo $DATE
 OUTDIR=dump-$DATE
 rm -rf $OUTDIR
+echo $MONGO_URL
+echo $PASSWORD
 mongodump --username=nailbiter --password=$PASSWORD "$MONGO_URL"  --gzip --out=$OUTDIR
