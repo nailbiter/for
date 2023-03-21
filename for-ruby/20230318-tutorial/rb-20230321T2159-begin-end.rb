a431386a35da006c509d7ba7efdded3f836a2b9b
Oleksii Leontiev <alozz1991@gmail.com>
2023-03-21 22:23:36 +0900

    stash

---
+++ for-ruby/20230318-tutorial/rb-20230321T2159-begin-end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+puts "This is main Ruby Program"
+
+END {
+   puts "Terminating Ruby Program"
+}
+
+# puts "hi"
+
+=begin
+this is
+a
+multiline
+comment
+=end
+
+BEGIN {
+   puts "Initializing Ruby Program"
+}
---
+++ for-ruby/20230318-tutorial/rb-20230321T2214-classes.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby -w
+
+# Class Vehicle {
+
+#    Number no_of_wheels
+#    Number horsepower
+#    Characters type_of_tank
+#    Number Capacity
+#    Function speeding {
+#    }
+   
+#    Function driving {
+#    }
+   
+#    Function halting {
+#    }
+# }
+
+
+# class Customer
+# end
+
+class Customer
+  # class variable
+  @@no_of_customers = 0
+
+  def initialize(id,name,addr)
+    @cust_id = id
+    @cust_name = name
+    @cust_addr = addr
+  end
+
+  def hello
+    puts "Hi, Ruby!"
+  end
+end
+
+## if `initialize` above would be undef, this would work
+# cust1 = Customer. new
+# cust2 = Customer.new
+
+cust3 = Customer.new("1","John","Wisdom Apartment")
+cust3.hello
