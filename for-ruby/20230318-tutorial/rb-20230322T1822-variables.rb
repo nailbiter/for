c142679f11de69354db49dba2904709070e8498e
Oleksii Leontiev <alozz1991@gmail.com>
2023-03-24 18:59:14 +0900

    ruby

--- for-ruby/20230318-tutorial/rb-20230322T1822-variables.rb
+++ for-ruby/20230318-tutorial/rb-20230322T1822-variables.rb
@@ -21,14 +21,26 @@ class2obj.print_global
 
 
 class Customer
+  @@no_of_customers = 0
   def initialize(id,name,addr)
+    @@no_of_customers +=1
     @cust_id = id
     @cust_name = name
     @cust_addr = addr
   end
   def display_details()
-    puts "customer id #$cust_id"
-    puts "customer id #$cust_id"
-    puts "customer id #$cust_id"
+    puts "customer id #@cust_id"
+    puts "customer name #@cust_name"
+    puts "customer addr #@cust_addr"
+  end
+  def total_no_of_customers()
+    puts "total num of customers #@@no_of_customers"
   end
 end
+
+c1 = Customer.new("1","abel","hanaan")
+c2 = Customer.new("2","kain","hanaan")
+c1.display_details
+c2.display_details
+c1.total_no_of_customers
+c1.total_no_of_customers
