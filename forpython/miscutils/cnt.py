34394b54164a7a555217c0ff2f97f9289c093abc
Oleksii Leontiev <alozz1991@gmail.com>
2023-06-23 22:47:15 +0900

    cnt

--- forpython/miscutils/cnt.py
+++ forpython/miscutils/cnt.py
@@ -17,6 +17,12 @@ ORGANIZATION:
      CREATED: 2023-05-11T21:10:09.417099
     REVISION: ---
 
+sample migration workflow:
+    sqlite> create table cnt2 as select *,"" as comment from cnt;
+    sqlite> select * from cnt2;
+    ...
+    sqlite> drop table cnt;
+    sqlite> create table cnt as select *,"" as comment from cnt2;
 ==============================================================================="""
 
 from dotenv import load_dotenv
@@ -40,6 +46,7 @@ from sqlalchemy import (
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 import functools
+import operator
 
 Base = declarative_base()
 option_with_envvar_explicit = functools.partial(click.option, show_envvar=True)
@@ -52,12 +59,14 @@ class Cnt(Base):
     creation_date = Column(DateTime)
     idx = Column(Integer)
     cnt_name = Column(String)
+    comment = Column(String)
 
-    def __init__(self, idx, cnt_name):
+    def __init__(self, idx, cnt_name, comment=""):
         self.uuid = str(uuid.uuid4())
         self.creation_date = datetime.now()
         self.idx = idx
         self.cnt_name = cnt_name
+        self.comment = comment
 
     def to_dict(self):
         return {
@@ -67,6 +76,7 @@ class Cnt(Base):
                 "creation_date",
                 "idx",
                 "cnt_name",
+                "comment",
             ]
         }
 
@@ -110,24 +120,42 @@ def _get_cnt_df(session):
 
 @cnt.command()
 @click.pass_context
-def ls(ctx):
+@option_with_envvar_explicit("-n", "--name")
+@option_with_envvar_explicit("-l/ ", "--long-form/--no-long-form", default=False)
+def ls(ctx, name, long_form):
+    # TODO: implement when `name` is given, print only value
+    # TODO: long form: show all, not only max
     session = ctx.obj["session"]
-    df = _get_cnt_df(session)
-    click.echo(df)
+    if long_form:
+        df = pd.DataFrame(map(operator.methodcaller("to_dict"), session.query(Cnt)))
+        df.set_index(["cnt_name", "idx"], inplace=True)
+        df.sort_index(inplace=True)
+        if name is not None:
+            df = df.loc[name]
+        click.echo(df)
+    else:
+        df = _get_cnt_df(session)
+        if name is None:
+            click.echo(df)
+        else:
+            click.echo(df.loc[name, "cnt"])
 
 
 @cnt.command()
 @option_with_envvar_explicit("-n", "--name", required=True)
 @option_with_envvar_explicit("-i", "--init-count", type=int, default=1)
 @option_with_envvar_explicit("-c", "--increment", type=click.IntRange(min=1), default=1)
+@option_with_envvar_explicit("-m", "--comment", default="")
 @click.pass_context
-def inc(ctx, name, init_count, increment):
+def inc(ctx, name, init_count, increment, comment):
     session = ctx.obj["session"]
     df = _get_cnt_df(session)
     if name in df.index:
-        cnt_ = Cnt(idx=int(df.loc[name, "cnt"]) + increment, cnt_name=name)
+        cnt_ = Cnt(
+            idx=int(df.loc[name, "cnt"]) + increment, cnt_name=name, comment=comment
+        )
     else:
-        cnt_ = Cnt(idx=init_count, cnt_name=name)
+        cnt_ = Cnt(idx=init_count, cnt_name=name, comment=comment)
     logging.warning(f"insert {cnt_}")
     session.add(cnt_)
     session.commit()
