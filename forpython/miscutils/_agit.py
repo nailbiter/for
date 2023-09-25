53dc5d21509187eff60edf85bac2a79be5ef6ddb
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-25 20:34:00 +0900

    forpython/miscutils/agit

--- forpython/miscutils/_agit.py
+++ forpython/miscutils/_agit.py
@@ -19,8 +19,21 @@ ORGANIZATION:
 ==============================================================================="""
 
 import uuid
+import logging
 
 
-def get_random_fn(ext=".txt"):
+def get_random_fn(ext: str = ".txt") -> str:
     assert ext.startswith(".")
     return f"/tmp/{uuid.uuid4()}{ext}"
+
+
+def filter_safe(
+    msg: str,
+    sensitive_info: list[str] = [],
+    sensitive_info_placeholder: str = "***",
+    **_,
+) -> str:
+    logging.warning(f"sensitive_info: {sensitive_info}")
+    for si in sensitive_info:
+        msg = msg.replace(si, sensitive_info_placeholder)
+    return msg
--- forpython/miscutils/agit.py
+++ forpython/miscutils/agit.py
@@ -35,6 +35,9 @@ from jinja2 import Template
 import subprocess
 import shlex
 import _agit
+import json5
+
+moption = functools.partial(click.option, show_envvar=True)
 
 
 def _add_logger(f):
@@ -42,6 +45,7 @@ def _add_logger(f):
 
     def _f(*args, **kwargs):
         return f(*args, logger=logger, **kwargs)
+
     _f.__name__ = f.__name__
     return _f
 
@@ -60,15 +64,21 @@ class _System:
 
 
 @click.group()
-@click.option("--dry-run/--no-dry-run", default=False)
-@click.option("--debug/--no-debug", default=False)
+@moption("--dry-run/--no-dry-run", default=False)
+@moption("--debug/--no-debug", default=False)
+@moption("-c", "--config-file", type=click.Path())
 @click.pass_context
-def git_copy(ctx, debug, **kwargs):
+def agit(ctx, debug, config_file, **kwargs):
     if debug:
         logging.basicConfig(level=logging.INFO)
     ctx.ensure_object(dict)
     ctx.obj["system"] = _System(kwargs["dry_run"])
     ctx.obj["kwargs"] = kwargs
+
+    ctx.obj["config"] = {}
+    if config_file is not None:
+        with open(config_file) as f:
+            ctx.obj["config"] = json5.load(f)
 
 
 class _GitignoreMatcher:
@@ -102,9 +112,9 @@ def _find_git_path(path_in="."):
     elif path.isfile(path_in):
         _path = path.dirname(path_in)
     else:
-        raise NotImplementedError(f"\"{path_in}\"")
+        raise NotImplementedError(f'"{path_in}"')
     # FIXME: this probably can be done better
-#    while True:
+    #    while True:
     repo = None
     for i in range(200):
         logging.info(f"#{i}: _path: {_path}")
@@ -118,33 +128,39 @@ def _find_git_path(path_in="."):
 
 
 def _get_head_sha(git_path=None):
-    repo, _path = _find_git_path(
-        **({} if git_path is None else {"path_in": git_path}))
+    repo, _path = _find_git_path(**({} if git_path is None else {"path_in": git_path}))
     assert not repo.bare
     head_commit = repo.head.commit
     diff = head_commit.diff(None)
-    assert(not diff), ("should be no changes on tree (do `git commit -a`)", diff, _path)
+    assert not diff, ("should be no changes on tree (do `git commit -a`)", diff, _path)
 
     return head_commit
 
 
-@git_copy.command()
+@agit.command()
 @click.argument("from_directory", type=click.Path(), nargs=-1)
 @click.argument("to_directory", type=click.Path(), nargs=1)
-@click.option("--db-filename", default=".git-copy.db")
-@click.option("-r", "--recursive", is_flag=True)
-@click.option("--check-clean-git-tree/--no-check-clean-git-tree", default=True)
+@moption("--db-filename", default=".git-copy.db")
+@moption("-r", "--recursive", is_flag=True)
+@moption("--check-clean-git-tree/--no-check-clean-git-tree", default=True)
 @click.pass_context
 @_add_logger
-def cp(ctx, from_directory, to_directory, db_filename, recursive, check_clean_git_tree, logger=None):
+def cp(
+    ctx,
+    from_directory,
+    to_directory,
+    db_filename,
+    recursive,
+    check_clean_git_tree,
+    logger=None,
+):
     repo, repo_root = _find_git_path()
     from_repo_roots = [_find_git_path(fd)[1] for fd in from_directory]
     logging.info(f"from_repo_roots: {from_repo_roots}")
     assert len(set(from_repo_roots)) == 1, from_repo_roots
-    is_another_repo = path.abspath(
-        repo_root) != path.abspath(from_repo_roots[0])
+    is_another_repo = path.abspath(repo_root) != path.abspath(from_repo_roots[0])
 
-    from_head_commit, remote_url = [None]*2
+    from_head_commit, remote_url = [None] * 2
     head_commit = None
     if check_clean_git_tree:
         head_commit = _get_head_sha(repo_root).hexsha
@@ -154,10 +170,11 @@ def cp(ctx, from_directory, to_directory, db_filename, recursive, check_clean_gi
             from_head_commit = _get_head_sha(from_repo_roots[0]).hexsha
             # FIXME: rewrite in Python
             remote_url = subprocess.getoutput(
-                f"cd {from_repo_roots[0]} && git remote get-url origin")
+                f"cd {from_repo_roots[0]} && git remote get-url origin"
+            )
             logging.info(f"remote_url: {remote_url}")
-#    print((repo_root, from_repo_roots, is_another_repo, from_head_commit, remote_url))
-#    exit(0)
+    #    print((repo_root, from_repo_roots, is_another_repo, from_head_commit, remote_url))
+    #    exit(0)
 
     cmd = "cp"
     if recursive:
@@ -165,21 +182,29 @@ def cp(ctx, from_directory, to_directory, db_filename, recursive, check_clean_gi
     _system = ctx.obj["system"]
     _system(f"{cmd} {' '.join(from_directory)} {to_directory}")
     _system(f"git add {to_directory}")
-    git_msg = Template("""
+    git_msg = (
+        Template(
+            """
 COPYCAT: "{{from}}" -> "{{to}}" {%if is_another_repo%}(remote {{remote_url}}:{{from_head_commit}}){%else%}({{head_commit}}){%endif%}
-    """).render({
-        "from": ([path.relpath(fd_, repo_root) for fd_ in from_directory]),
-        "to": (path.relpath(to_directory, repo_root)),
-        "is_another_repo": is_another_repo,
-        "from_head_commit": from_head_commit,
-        "remote_url": remote_url,
-        "head_commit": head_commit,
-    }).strip()
+    """
+        )
+        .render(
+            {
+                "from": ([path.relpath(fd_, repo_root) for fd_ in from_directory]),
+                "to": (path.relpath(to_directory, repo_root)),
+                "is_another_repo": is_another_repo,
+                "from_head_commit": from_head_commit,
+                "remote_url": remote_url,
+                "head_commit": head_commit,
+            }
+        )
+        .strip()
+    )
     _random_fn = _agit.get_random_fn()
     with open(_random_fn, "w") as f:
-        f.write(git_msg)
+        f.write(_agit.filter_safe(git_msg, **ctx.obj["config"]))
     _system(f"""git commit -a -F \"{_random_fn}\" """)
 
 
 if __name__ == "__main__":
-    git_copy()
+    agit(auto_envvar_prefix="AGIT", show_default=True)
