04598560ff3829fdc4e0ef221e95c420698d2519
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-28 21:18:16 +0900

    miscutils/jira-cli/api/issue/add

--- forpython/miscutils/_jira_cli.py
+++ forpython/miscutils/_jira_cli.py
@@ -23,6 +23,7 @@ from jinja2 import Template
 import click
 from requests.auth import HTTPBasicAuth
 import typing
+from datetime import datetime, timedelta
 
 
 def ssj(s: str) -> str:
@@ -60,8 +61,108 @@ def run_cmd(cmd, logger=logging):
 
 def api_init(ctx_obj: dict, path: str) -> typing.Tuple[str, str]:
     """
-    @return (str,str) url,auth
+    @return (str,str) (url, auth)
     """
     url = f"https://{ctx_obj['jira_url']}/rest/api/latest/{path}"
     auth = HTTPBasicAuth(ctx_obj["jira_email"], ctx_obj["jira_api_token"])
     return url, auth
+
+
+def get_add_issue_payload(
+    project_id: int,
+    summary: str,
+    issue_type_id: str,
+    labels: list[str] = [],
+    custom_fields: list[typing.Tuple[str, str]] = [],
+    due_date: typing.Optional[datetime] = None,
+    reporter_id: typing.Optional[str] = None,
+    assignee_id: typing.Optional[str] = None,
+    parent_id: typing.Optional[str] = None,
+):
+    payload = {
+        "fields": {
+            ## ??
+            # "environment": {
+            #     "content": [
+            #         {
+            #             "content": [{"text": "UAT", "type": "text"}],
+            #             "type": "paragraph",
+            #         }
+            #     ],
+            #     "type": "doc",
+            #     "version": 1,
+            # },
+            # "fixVersions": [{"id": "10001"}],
+            # "priority": {"id": "20000"},
+            # "security": {"id": "10000"},
+            ## todo
+            ## opt
+            **{k: v for k, v in list(custom_fields)},
+            # "customfield_10000": "09/Jun/19",
+            # "customfield_20000": "06/Jul/19 3:25 PM",
+            # "customfield_30000": ["10000", "10002"],
+            ## opt todo
+            # "description": {
+            #     "content": [
+            #         {
+            #             "content": [
+            #                 {
+            #                     "text": "Order entry fails when selecting supplier.",
+            #                     "type": "text",
+            #                 }
+            #             ],
+            #             "type": "paragraph",
+            #         }
+            #     ],
+            #     "type": "doc",
+            #     "version": 1,
+            # },
+            # "customfield_40000": {
+            #     "content": [
+            #         {
+            #             "content": [
+            #                 {"text": "Occurs on all orders", "type": "text"}
+            #             ],
+            #             "type": "paragraph",
+            #         }
+            #     ],
+            #     "type": "doc",
+            #     "version": 1,
+            # },
+            # "customfield_50000": {
+            #     "content": [
+            #         {
+            #             "content": [
+            #                 {
+            #                     "text": "Could impact day-to-day work.",
+            #                     "type": "text",
+            #                 }
+            #             ],
+            #             "type": "paragraph",
+            #         }
+            #     ],
+            #     "type": "doc",
+            #     "version": 1,
+            # },
+            # "customfield_70000": ["jira-administrators", "jira-software-users"],
+            # "customfield_80000": {"value": "red"},
+            # "components": [{"id": "10000"}],
+            # "timetracking": {"originalEstimate": "10", "remainingEstimate": "5"},
+            # "versions": [{"id": "10000"}],
+            ## done
+            # "parent": {"key": "PROJ-123"},
+            **({} if parent_id is None else dict(parent=dict(key=parent_id))),
+            # "duedate": "2019-05-11",
+            **({} if due_date is None else dict(duedate=due_date.strftime("%Y-%m-%d"))),
+            "summary": summary,
+            "labels": list(labels),
+            "project": {"id": str(project_id)},  # 1
+            # "reporter": {"id": "5b10a2844c20165700ede21g"},
+            # "assignee": {"id": "5b109f2e9729b51b54dc274d"},
+            **({} if reporter_id is None else dict(reporter={"id": reporter_id})),
+            **({} if assignee_id is None else dict(assignee={"id": assignee_id})),
+            "issuetype": {"id": str(issue_type_id)},
+        },
+        "update": {},
+    }
+    return payload
--- forpython/miscutils/jira-cli.py
+++ forpython/miscutils/jira-cli.py
@@ -35,7 +35,7 @@ import logging
 import functools
 import itertools
 from jinja2 import Template
-from _jira_cli import run_cmd, make_cmd, ssj, api_init
+from _jira_cli import run_cmd, make_cmd, ssj, api_init, get_add_issue_payload
 import requests
 from requests.auth import HTTPBasicAuth
 import json
@@ -70,118 +70,117 @@ def api(ctx, **kwargs):
     """
     ctx.obj = {**ctx.obj, **kwargs}
 
+
 @api.group(name="project")
 @click.pass_context
 def api_project(ctx):
     pass
 
+
 @api_project.command()
+@_build_click_options
+@moption("--simplify/--no-simplify", "-s/ ", default=False)
 @click.pass_context
-def ls(ctx):
-    pass
+def ls(ctx, simplify, **format_df_kwargs):
+    # This code sample uses the 'requests' library:
+    # http://docs.python-requests.org
+
+    url, auth = api_init(ctx.obj, "project/search")
+    headers = {"Accept": "application/json"}
+    response = requests.request("GET", url, headers=headers, auth=auth)
+
+    df = pd.DataFrame(json.loads(response.text)["values"])
+    if simplify:
+        df = df[["id", "name"]]
+    click.echo(apply_click_options(df, format_df_kwargs))
+
 
 @api.group(name="issue")
 @click.pass_context
 def api_issue(ctx):
+    pass
+
+
+@api.group(name="issue-type")
+@click.pass_context
+def api_issue_type(ctx):
+    pass
+
+
+@api_issue_type.command(name="ls")
+@_build_click_options
+@moption("--simplify/--no-simplify", "-s/ ", default=False)
+@click.pass_context
+def api_issue_type_ls(ctx, simplify, **format_df_kwargs):
+    # This code sample uses the 'requests' library:
+    # http://docs.python-requests.org
+
+    url, auth = api_init(ctx.obj, "issuetype")
+    headers = {"Accept": "application/json"}
+    response = requests.request("GET", url, headers=headers, auth=auth)
+
+    # logging.warning(response.text)
+    df = pd.DataFrame(json.loads(response.text))
+    if simplify:
+        df = df[["id", "name", "scope"]]
+
+    click.echo(apply_click_options(df, format_df_kwargs))
+
+
+@api.group(name="user")
+@click.pass_context
+def api_user(ctx):
+    pass
+
+
+@api_user.command(name="ls")
+@_build_click_options
+@moption("--simplify/--no-simplify", "-s/ ", default=False)
+@click.pass_context
+def api_user_ls(ctx, simplify, **format_df_kwargs):
+    url, auth = api_init(ctx.obj, "users/search")
+    headers = {"Accept": "application/json"}
+
+    response = requests.request("GET", url, headers=headers, auth=auth)
+    df = pd.DataFrame(json.loads(response.text))
+    if simplify:
+        df = df[["accountId", "displayName", "emailAddress"]]
+    click.echo(apply_click_options(df, format_df_kwargs))
 
 
 @api_issue.command()
 @click.pass_context
-def add(ctx):
+def edit(ctx):
+    pass
+
+
+@api_issue.command(name="add")
+@moption("-p", "--project-id", type=int, required=True)
+@moption("-t", "--issue_type_id", type=int, required=True)
+@moption("-s", "--summary", type=str, required=True)
+@moption("-l", "--label", "labels", type=str, multiple=True)
+@moption("-c", "--custom-field", "custom_fields", type=(str, str), multiple=True)
+@moption("-d", "--due-date", type=click.DateTime(["%Y-%m-%d"]))
+@moption("-r", "--reporter-id", type=str)
+@moption("-a", "--assignee-id", type=str)
+@moption("-P", "--parent-id", type=str)
+@click.pass_context
+def api_issue_add(ctx, **kwargs):
     """
     https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-post
     """
     # jira-cli.py wrap -p JC issue create -t Task -P JC-1 -s 'closing a sprint and view reports'
 
-    
     # This code sample uses the 'requests' library:
     # http://docs.python-requests.org
 
     url, auth = api_init(ctx.obj, "issue")
     headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
-    payload = json.dumps(
-        {
-            "fields": {
-                "assignee": {"id": "5b109f2e9729b51b54dc274d"},
-                "components": [{"id": "10000"}],
-                "customfield_10000": "09/Jun/19",
-                "customfield_20000": "06/Jul/19 3:25 PM",
-                "customfield_30000": ["10000", "10002"],
-                "customfield_40000": {
-                    "content": [
-                        {
-                            "content": [
-                                {"text": "Occurs on all orders", "type": "text"}
-                            ],
-                            "type": "paragraph",
-                        }
-                    ],
-                    "type": "doc",
-                    "version": 1,
-                },
-                "customfield_50000": {
-                    "content": [
-                        {
-                            "content": [
-                                {
-                                    "text": "Could impact day-to-day work.",
-                                    "type": "text",
-                                }
-                            ],
-                            "type": "paragraph",
-                        }
-                    ],
-                    "type": "doc",
-                    "version": 1,
-                },
-                "customfield_60000": "jira-software-users",
-                "customfield_70000": ["jira-administrators", "jira-software-users"],
-                "customfield_80000": {"value": "red"},
-                "description": {
-                    "content": [
-                        {
-                            "content": [
-                                {
-                                    "text": "Order entry fails when selecting supplier.",
-                                    "type": "text",
-                                }
-                            ],
-                            "type": "paragraph",
-                        }
-                    ],
-                    "type": "doc",
-                    "version": 1,
-                },
-                "duedate": "2019-05-11",
-                "environment": {
-                    "content": [
-                        {
-                            "content": [{"text": "UAT", "type": "text"}],
-                            "type": "paragraph",
-                        }
-                    ],
-                    "type": "doc",
-                    "version": 1,
-                },
-                "fixVersions": [{"id": "10001"}],
-                "issuetype": {"id": "10000"},
-                "labels": ["bugfix", "blitz_test"],
-                "parent": {"key": "PROJ-123"},
-                "priority": {"id": "20000"},
-                "project": {"id": "10000"}, #1
-                "reporter": {"id": "5b10a2844c20165700ede21g"},
-                "security": {"id": "10000"},
-                "summary": "Main order flow broken",
-                "timetracking": {"originalEstimate": "10", "remainingEstimate": "5"},
-                "versions": [{"id": "10000"}],
-            },
-            "update": {},
-        }
-    )
+    payload = get_add_issue_payload(**kwargs)
+    payload = json.dumps(payload)
 
     response = requests.request("POST", url, data=payload, headers=headers, auth=auth)
-
     click.echo(
         json.dumps(
             json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
