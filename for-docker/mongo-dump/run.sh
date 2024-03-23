69570512437c9642c21baf649e332c7bd00dbb1b
Alex Leontiev <alozz1991@gmail.com>
2024-03-23 21:19:47 +0900

    stash

--- for-docker/mongo-dump/run.sh
+++ for-docker/mongo-dump/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /snap/bin/docker run --env-file=`pwd`/.env -v `pwd`/output/:/output:rw nailbiter/mongo-dump
+/snap/bin/docker run --env-file=`pwd`/.env-gstasks -v `pwd`/output-gstasks/:/output:rw nailbiter/mongo-dump
 #docker run -v `pwd`/notebooks:/notebooks:ro -v `pwd`/pdfs:/pdfs:rw -it nailbiter/jupyter-latex-pdf:latest
