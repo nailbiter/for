e41d7b57dbb97be7662343d9daf4f64e8fae64cd
Oleksii Leontiev <alozz1991@gmail.com>
2025-02-08 22:04:51 +0900

    aprompt

--- forpython/miscutils/aprompt.py
+++ forpython/miscutils/aprompt.py
@@ -17,6 +17,8 @@ ORGANIZATION:
      CREATED: 2025-01-26T12:50:30.958685
     REVISION: ---
 
+TODO:
+    1. 
 ==============================================================================="""
 
 import click
@@ -26,7 +28,8 @@ from os import path
 import logging
 import functools
 
-moption = functools.partial(click.option)
+moption = functools.partial(click.option, show_default=True, show_envvar=True)
+AVAILABLE_TEMPLATE_FORMATS = ["jinja2", "string_template"]
 
 
 @click.command()
@@ -44,11 +47,17 @@ moption = functools.partial(click.option)
     "--templates-dir",
     type=click.Path(exists=True, dir_okay=True, file_okay=False),
 )
-@moption("-t", "--template", type=str, required=True)
+@moption(
+    "-t",
+    "--template-filename",
+    type=click.Path(),
+    required=True,
+    help="relative if `--templates-dir` is given",
+)
 @moption("-p", "--param", "params", multiple=True, type=(str, str))
 @moption(
     "--template-format",
-    type=click.Choice(["jinja2", "string_template", "auto"]),
+    type=click.Choice([*AVAILABLE_TEMPLATE_FORMATS, "auto"]),
     default="auto",
 )
 @moption(
@@ -57,17 +66,39 @@ moption = functools.partial(click.option)
 @moption(
     "-E", "--prompt-engine", type=click.Choice(["gemini", "chatgpt"]), required=True
 )
+@moption("-T", "--engine-access-token", required=True)
 def aprompt(
     logging_config,
     prompt_engine,
     cache_config,
     templates_dir,
     params,
-    template,
+    template_filename,
     template_format,
     post_processors,
+    engine_access_token,
 ):
-    pass
+    if templates_dir is None:
+        templates_dir, _ = path.split(template_filename)
+    else:
+        template_filename = path.join(templates_dir, template_filename)
+    if template_format == "auto":
+        ## FIXME: fragile (but works for now)
+        i = template_filename.index(".")
+        ext = template_filename[i:]
+        logging.warning(dict(ext=ext))
+        template_format = {".jinja.txt": "jinja2", ".txt": "string_template"}[ext]
+        assert template_format in AVAILABLE_TEMPLATE_FORMATS, (
+            template_format,
+            AVAILABLE_TEMPLATE_FORMATS,
+        )
+    logging.warning(
+        dict(
+            templates_dir=templates_dir,
+            template_filename=template_filename,
+            template_format=template_format,
+        )
+    )
 
 
 if __name__ == "__main__":
@@ -75,4 +106,4 @@ if __name__ == "__main__":
     if path.isfile(fn):
         logging.warning(f"loading `{fn}`")
         load_dotenv(dotenv_path=fn)
-    aprompt()
+    aprompt(auto_envvar_prefix="APROMPT")
