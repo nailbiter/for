76b6fe026af9dcd29439f48f26c43b552934862f
Oleksii Leontiev <alozz1991@gmail.com>
2025-02-08 21:43:25 +0900

    aprompt

---
+++ forpython/_aprompt/__init__.py
@@ -0,0 +1,19 @@
+"""===============================================================================
+
+        FILE: /Users/nailbiter/for/forpython/_aprompt/__init__.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2025-02-08T21:42:35.325126
+    REVISION: ---
+
+==============================================================================="""
