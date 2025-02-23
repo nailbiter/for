cbdeebd8deee06a5c5207745944797ead828519f
Oleksii Leontiev <alozz1991@gmail.com>
2025-02-23 15:10:50 +0900

    jira-cli

--- for-jira-playground/scripts/issues-to-scripts.sh
+++ for-jira-playground/scripts/issues-to-scripts.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-jira-cli.py api jql -j 'project = ML3 AND status IN ("To Do", "In Progress") AND sprint != empty ORDER BY created DESC' -o json|jq -f jq/issues-by-sprints.jq
+jira-cli.py api jql -s 0 -m 100 -j 'project = ML3 AND status IN ("To Do", "In Progress") AND sprint != empty ORDER BY created DESC' -o json|jq -f jq/issues-by-sprints.jq|jq '.|sort'
+jira-cli.py api jql -s 100 -m 100 -j 'project = ML3 AND status IN ("To Do", "In Progress") AND sprint != empty ORDER BY created DESC' -o json|jq -f jq/issues-by-sprints.jq|jq '.|sort'
