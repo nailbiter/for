e3d8be57e3153318e75001fc6a10962834876fc0
Oleksii Leontiev <alozz1991@gmail.com>
2025-03-06 20:03:00 +0900

    translate

--- forpython/miscutils/translate.py
+++ forpython/miscutils/translate.py
@@ -20,21 +20,85 @@ ORGANIZATION:
 ==============================================================================="""
 
 import click
-
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
+
+# import asyncio
 from os import path
 import logging
+import functools
+
+#! pip install google-cloud-translate
+#! pip install google-cloud-translate==2.0.1
+# from google.cloud import translate as google_translate
+from google.cloud import translate_v2 as google_translate
+
+# !pip install googletrans
+# from googletrans import Translator
+
+moption = functools.partial(click.option, show_envvar=True)
+
+KNOWN_FILE_FORMATS = {"pptx": None}
+
+KNOWN_LANGUAGES = {"es": None, "ja": None, "en": None}
 
 
 @click.command()
-def translate():
-    pass
+@moption("--input-file", "-i", type=click.Path(allow_dash=True), default="-")
+@moption(
+    "--input-format",
+    "-f",
+    type=click.Choice(["auto", *KNOWN_FILE_FORMATS]),
+    default="auto",
+)
+@moption(
+    "--input-language",
+    "-I",
+    type=click.Choice(list(KNOWN_LANGUAGES)),
+    required=True,
+    default="ja",
+)
+@moption(
+    "--output-language",
+    "-o",
+    type=click.Choice(list(KNOWN_LANGUAGES)),
+    required=True,
+    default="en",
+)
+def translate(input_file, input_format, input_language, output_language):
+    logging.warning(
+        dict(input_language=input_language, output_language=output_language)
+    )
+    with click.open_file(input_file) as f:
+        text = f.read().strip()
+    logging.warning(f"i: {text}")
+    # client = translate.TranslationServiceClient()
+    # response = client.translate_text(
+    #     request={
+    #         "parent": parent,
+    #         "contents": [text],
+    #         "mime_type": "text/plain",  # You can also use "text/html"
+    #         "target_language_code": output_language,
+    #     }
+    # )
+    # translation, *_ = list(response.translation)
+    # text = translation.translated_text
+
+    # translator = Translator()
+    # text = translator.translate(text, src=input_language, dest=output_language).text
+
+    client = google_translate.Client()
+    result = client.translate(
+        text, source_language=input_language, target_language=output_language
+    )["translatedText"]
+
+    logging.warning(f"o: {text}")
+    click.echo(text)
 
 
 if __name__ == "__main__":
-    #    fn = ".env"
-    #    if path.isfile(fn):
-    #        logging.warning(f"loading `{fn}`")
-    #        load_dotenv(dotenv_path=fn)
-    translate()
+    fn = ".env"
+    if path.isfile(fn):
+        logging.warning(f"loading `{fn}`")
+        load_dotenv(dotenv_path=fn)
+    translate(show_default=True, auto_envvar_prefix="TRANSLATE")
