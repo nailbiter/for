7a753410d54106fdee48f0eff6fa3c950da5da64
Oleksii Leontiev <alozz1991@gmail.com>
2025-05-30 00:00:20 +0900

    stash

--- forpython/miscutils/aprompt.py
+++ forpython/miscutils/aprompt.py
@@ -81,6 +81,7 @@ def aprompt(ctx, engine_access_token):
     type=click.Choice(
         [
             "gemini-1.5-flash-latest",
+            "gemini-1.5-pro",
             "gemini-2.5-pro-preview-05-06",
             "gemini-2.5-flash-preview-04-17",
         ]
