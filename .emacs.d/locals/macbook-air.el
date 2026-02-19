0442fb22662a4c818fdb1b7ca985f84680e753d6
Oleksii Leontiev <alozz1991@gmail.com>
2026-02-18 21:38:50 -0500

    emacs

--- .emacs.d/locals/macbook-air.el
+++ .emacs.d/locals/macbook-air.el
@@ -19,9 +19,21 @@
                (display-buffer-reuse-window display-buffer-same-window)))
 
 ;; auto-revert
-(global-auto-revert-mode 1)
+;(global-auto-revert-mode 1)
+;; (with-eval-after-load 'autorevert
+;;   (global-auto-revert-mode 1))
 ;; File notifications: Emacs can use native file system notifications (like inotify on Linux or kqueue on macOS) to detect changes instantly rather than polling. To rely solely on notifications and avoid periodic polling (which can save power), you can set auto-revert-avoid-polling to t.
 ;(setq auto-revert-avoid-polling t)
 ;; Non-file buffers: By default, global auto-revert mode only affects buffers associated with files on disk. To enable auto-reverting for other buffer types where it is implemented (such as Dired buffers or the Buffer Menu), set global-auto-revert-non-file-buffers to t.
 ;(setq global-auto-revert-non-file-buffers t)
-
+(use-package autorevert
+  :ensure nil ; built-in
+  :config
+  (global-auto-revert-mode 1)
+  (setq auto-revert-verbose t) ; Optional: tells you in the echo area when a file reverts
+  (setq global-auto-revert-non-file-buffers t))
+(with-eval-after-load 'autorevert
+  ;; This ensures the lighter " ARev" is visible even in global mode
+  (setq global-auto-revert-mode-text " ARev")
+  ;; Force the minor mode alist to recognize the global state for display
+  (add-to-list 'minor-mode-alist '(global-auto-revert-mode " ARev")))
