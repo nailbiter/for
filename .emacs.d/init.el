a02e45d77401c24d5764e6ac3a9c4b5af61b3067
Oleksii Leontiev <alozz1991@gmail.com>
2026-04-12 20:15:26 +0900

    no-emacs-warning

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,24 @@
 ;;; --- 1. CORE PATH & ENVIRONMENT SETUP ---
-;; This ensures all your Homebrew tools (GPG, SBCL, etc.) are found on M-series Macs
+;; PREPEND your custom paths to the system path so we don't lose /usr/bin/as
 (let ((my-paths '("/opt/homebrew/bin" 
+                  "/opt/homebrew/sbin"
                   "/usr/local/bin" 
                   "/Users/nailbiter/for/forpython/miscutils/")))
-  (setenv "PATH" (mapconcat 'identity my-paths ":"))
+  (setenv "PATH" (concat (mapconcat 'identity my-paths ":") ":" (getenv "PATH")))
   (setq exec-path (append my-paths exec-path)))
 
 ;;; --- 2. CUSTOM SET VARIABLES ---
 (custom-set-variables
  '(custom-safe-themes
    '("fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default))
- '(epg-gpg-program "/opt/homebrew/bin/gpg") ; Updated for M-series
+ '(epg-gpg-program "/opt/homebrew/bin/gpg")
+ '(prettier-js-command "/opt/homebrew/bin/prettier")
  '(package-selected-packages
    '(dockerfile-mode dracula-theme drag-stuff elisa git-modes go-mode
      haskell-mode indent-tools js-format json-mode json-reformat
      markdown-mode pass password-store password-store-otp
      prettier-js python-black slime slime-volleyball use-package
-     whole-line-or-region yaml yaml-mode))
- '(prettier-js-command "/opt/homebrew/bin/prettier")) ; Updated for M-series
+     whole-line-or-region yaml yaml-mode)))
 
 (custom-set-faces)
 
@@ -36,7 +37,7 @@
   (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t) ; This simplifies the blocks below
+(setq use-package-always-ensure t)
 
 ;;; --- 4. THEME & VISUALS ---
 (use-package dracula-theme
@@ -47,7 +48,7 @@
 (show-paren-mode 1)
 (global-display-line-numbers-mode)
 
-;;; --- 5. EDITOR TOOLS (DRAG-STUFF, WRAP, ETC) ---
+;;; --- 5. EDITOR TOOLS ---
 (use-package drag-stuff
   :ensure t
   :bind (("<ESC> <up>" . drag-stuff-up)
@@ -65,33 +66,26 @@
   :config
   (wrap-region-global-mode 1))
 
-;;; --- 6. LANGUAGES & BINARIES ---
-;; Updated binary paths for M-series
+;;; --- 6. LANGUAGES & BINARIES (ARM64 PATHS) ---
 (setq inferior-lisp-program "/opt/homebrew/bin/sbcl")
 (setq ispell-program-name "/opt/homebrew/bin/aspell")
 (setq shell-file-name "/opt/homebrew/bin/fish")
 (setq markdown-command "/opt/homebrew/bin/multimarkdown")
 (setq epa-pinentry-mode 'loopback)
 
-;; Yasnippet
 (use-package yasnippet
   :ensure t
-  :init
-  (add-to-list 'load-path "~/.emacs.d/yasnippet")
   :config
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   (yas-global-mode 1))
 
-;; Python & Black
 (use-package python-black
   :ensure t
   :after python
   :hook (python-mode . python-black-on-save-mode-enable-dwim)
   :config
-  ;; Note: check 'which black' in terminal. If using brew, use the path below.
   (setq python-black-command "/opt/homebrew/bin/black"))
 
-;; YAML & Markdown
 (use-package yaml-mode
   :ensure t
   :mode ("\\.yml\\'" . yaml-mode))
@@ -109,27 +103,23 @@
         (t (self-insert-command (or arg 1)))))
 (global-set-key "%" 'match-paren)
 
-;; Hippie Expand
 (global-set-key "\C-n" 'hippie-expand)
 (global-set-key "\C-c\C-f" 'json-pretty-print-buffer-ordered)
 
-;; pbcopy
 (defun pbcopy ()
-  "Copy the entire buffer content to the macOS clipboard using pbcopy."
+  "Copy the entire buffer content to the macOS clipboard."
   (interactive)
   (call-process-region (point-min) (point-max) "pbcopy")
   (message "Buffer content copied to clipboard."))
 
-;; Python Init
 (defun python-init (template)
   (interactive "Mtemplate: ")
   (progn
     (shell-command (format "~/for/forpython/new_file.py new-file --no-silent \"%s\" \"%s\"" buffer-file-name template) nil nil)
     (revert-buffer :ignore-auto :noconfirm)))
 
-;; Gemini Integration
 (defun gemini ()
-  "Run a Python script with current buffer's content as stdin."
+  "Run a Python script with current buffer content as stdin."
   (interactive)
   (let* ((user-prompt (read-string "Enter Prompt: "))
          (current-buffer-content (buffer-substring-no-properties (point-min) (point-max)))
@@ -138,8 +128,7 @@
          (process-connection-type nil)
          (gemini-python-interpreter (or (bound-and-true-p gemini-python-interpreter) "/opt/homebrew/bin/python3")))
 
-    (when (get-buffer output-buffer-name)
-      (kill-buffer output-buffer-name))
+    (when (get-buffer output-buffer-name) (kill-buffer output-buffer-name))
 
     (with-temp-buffer
       (insert current-buffer-content)
@@ -163,7 +152,6 @@
 
 ;; Compile command by extension
 (defun my/set-compile-command-by-ext ()
-  "Sets the compile-command variable based on file extension."
   (when buffer-file-name
     (let ((ext (file-name-extension buffer-file-name))
           (fname (file-name-nondirectory buffer-file-name)))
@@ -172,11 +160,16 @@
         (setq-local compile-command (format "jinja %s | tee /dev/stderr | pbcopy" fname)))))))
 (add-hook 'find-file-hook 'my/set-compile-command-by-ext)
 
-;;; --- 8. LOCAL OVERRIDES ---
-;; Load local settings if the file exists (e.g., for work paths)
+;;; --- 8. LOCAL OVERRIDES & FINAL SETTINGS ---
 (let ((local-settings-file (expand-file-name "~/.emacs.d/local.el")))
   (when (file-exists-p local-settings-file)
     (load-file local-settings-file)))
 
 (setq require-final-newline t)
 (setq display-buffer-base-action '(display-buffer-in-tab))
+
+;; Stop the async native-comp warning buffer from popping up
+(setq native-comp-async-report-warnings-errors 'silent)
+
+;; Or, if you want to see errors but ignore these linting warnings:
+(setq warning-minimum-level :error)
